3549b71be61462a622fed21ce761e2b9ce54b9ad
sacundim <anybody@sacundim.org>
2020-05-02 23:40:09 -0700

    Initial check-in, has some data but still could use more

---
+++ postgres/020-load-data.sql
@@ -0,0 +1,7 @@
+COPY bulletin
+FROM '/data/PuertoRico-bulletin.csv'
+    CSV ENCODING 'UTF-8' HEADER NULL '';
+
+COPY bitemporal
+FROM '/data/PuertoRico-bitemporal.csv'
+    CSV ENCODING 'UTF-8' HEADER NULL '';
