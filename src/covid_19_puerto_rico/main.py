e21b6333d5258848c323f9eb5ace6a2ca14b05ea
Luis Casillas <luis@casillas.org>
2020-05-03 15:33:27 -0700

    Replace hardcoding with command line parsing.

--- src/covid_19_puerto_rico/main.py
+++ src/covid_19_puerto_rico/main.py
@@ -10,17 +10,26 @@ import seaborn as sns
 import sqlalchemy
 from sqlalchemy.sql import select
 
+def process_arguments():
+    parser = argparse.ArgumentParser(description='Generate Puerto Rico COVID-19 graphs')
+    parser.add_argument('--output-dir', type=str,
+                        help='Directory into which to place output')
+    parser.add_argument('--bulletin-date', type=datetime.date.fromisoformat,
+                        help='Bulletin date to generate graphs for')
+    return parser.parse_args()
+
 def main():
     logging.basicConfig(format='%(asctime)s %(message)s',
                         level=logging.INFO)
-    logging.info('Hello World!')
     sns.set(style="darkgrid")
+
+    args = process_arguments();
 
     engine = create_db()
     with engine.connect() as connection:
-        main_graph(connection, datetime.date(2020, 5, 2))
+        main_graph(connection, args)
 
-def main_graph(connection, bulletin_date):
+def main_graph(connection, args):
     meta = sqlalchemy.MetaData()
     table = sqlalchemy.Table('main_graph', meta, autoload=True, autoload_with=connection)
     query = select([table.c.datum_date,
@@ -29,7 +38,7 @@ def main_graph(connection, bulletin_date):
                     table.c.positive_results,
                     table.c.announced_cases,
                     table.c.deaths,
-                    table.c.announced_deaths]).where(table.c.bulletin_date == bulletin_date)
+                    table.c.announced_deaths]).where(table.c.bulletin_date == args.bulletin_date)
     df = pd.read_sql_query(query, connection)
     logging.info("%s", df['positive_results'])
 
@@ -38,8 +47,9 @@ def main_graph(connection, bulletin_date):
     graph.fig.autofmt_xdate()
     graph.set(yscale='log')
 
-    logging.info("Writing graph to main.png")
-    graph.savefig("output/main.png", dpi=150)
+    filename = f"{args.output_dir}/main_{args.bulletin_date}.png"
+    logging.info("Writing graph to %s", filename)
+    graph.savefig(filename, dpi=150)
 
 
 def create_db():
