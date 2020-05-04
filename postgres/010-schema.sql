56320e7b863bb381eb989261fe6be4ded6df1af0
sacundim <anybody@sacundim.org>
2020-05-03 17:37:06 -0700

    Add one data quality view to check announcement new vs cumulative (which does exibit off-by-one errors in the source).

--- postgres/010-schema.sql
+++ postgres/010-schema.sql
@@ -1,8 +1,8 @@
 CREATE FUNCTION log2(x NUMERIC)
 RETURNS NUMERIC AS $$
     SELECT log(2.0, x);
-$$ LANGUAGE SQL
-;
+$$ LANGUAGE SQL;
+
 
 CREATE TABLE bitemporal (
     bulletin_date DATE NOT NULL,
@@ -93,7 +93,7 @@ death certificate.  Given by date that they were announced (not
 date of actual death).  First reported April 8';
 
 
-CREATE VIEW bitemporal_analysis AS
+CREATE VIEW bitemporal_agg AS
 SELECT
     bulletin_date,
     datum_date,
@@ -134,7 +134,7 @@ WINDOW
     bulletin AS (PARTITION BY bulletin_date ORDER BY datum_date),
     datum AS (PARTITION BY datum_date ORDER BY bulletin_date);
 
-COMMENT ON VIEW bitemporal_analysis IS
+COMMENT ON VIEW bitemporal_agg IS
 'Useful aggregations/windows over the bitemporal table:
 
 - Cumulative: Cumulative sums, partitioned by bulletin date;
@@ -143,7 +143,63 @@ COMMENT ON VIEW bitemporal_analysis IS
 - Lateness score: Delta * (bulletin_date - datum_date).';
 
 
-CREATE VIEW main_graph AS
+-------------------------------------------------------------------------------
+CREATE SCHEMA quality;
+
+COMMENT ON SCHEMA quality IS
+'Views to explore data quality issues in the data';
+
+
+CREATE VIEW quality.mismatched_announcement_aggregates AS
+WITH base AS (
+	SELECT
+		bulletin_date,
+		lag(cumulative_cases) OVER bulletin
+			AS previous_cumulative_cases,
+		new_cases,
+		cumulative_cases - lag(cumulative_cases) OVER bulletin
+			AS computed_new_cases,
+		cumulative_cases,
+		lag(cumulative_cases) OVER bulletin + new_cases
+			AS computed_cumulative_cases,
+
+		lag(cumulative_confirmed_cases) OVER bulletin
+			AS previous_cumulative_confirmed_cases,
+		new_confirmed_cases,
+		cumulative_confirmed_cases - lag(cumulative_confirmed_cases) OVER bulletin
+			AS computed_new_confirmed_cases,
+		cumulative_confirmed_cases,
+		lag(cumulative_confirmed_cases) OVER bulletin + new_confirmed_cases
+			AS computed_cumulative_confirmed_cases,
+
+		lag(cumulative_probable_cases) OVER bulletin
+			AS previous_cumulative_probable_cases,
+		new_probable_cases,
+		cumulative_probable_cases - lag(cumulative_probable_cases) OVER bulletin
+			AS computed_new_probable_cases,
+		cumulative_probable_cases,
+		lag(cumulative_probable_cases) OVER bulletin + new_probable_cases
+			AS computed_cumulative_probable_cases
+	FROM announcement a
+	WINDOW bulletin AS (ORDER BY bulletin_date))
+SELECT *
+FROM base
+WHERE new_cases != computed_new_cases
+OR new_confirmed_cases != computed_new_confirmed_cases
+OR new_probable_cases != computed_new_probable_cases;
+
+COMMENT ON TABLE quality.mismatched_announcement_aggregates IS
+'Check whether the daily new cases and the cumulative figures
+in the bulletins match. Which they don''t always do...';
+
+
+-------------------------------------------------------------------------------
+CREATE SCHEMA products;
+
+COMMENT ON SCHEMA products IS
+'Views with queries that are intended to be presentations of the data';
+
+CREATE VIEW products.main_graph AS
 SELECT
 	ba.bulletin_date,
 	ba.datum_date,
@@ -154,12 +210,23 @@ SELECT
 	ba.cumulative_deaths AS deaths,
 	announcement.cumulative_cases AS announced_cases,
 	announcement.cumulative_deaths AS announced_deaths
-FROM bitemporal_analysis ba
+FROM bitemporal_agg ba
 FULL OUTER JOIN announcement
 	ON announcement.bulletin_date = ba.datum_date;
 
 
-CREATE VIEW lateness_analysis AS
+CREATE VIEW products.daily_deltas AS
+SELECT
+    bulletin_date,
+	datum_date,
+	delta_confirmed_and_probable_cases,
+	delta_confirmed_cases,
+	delta_probable_cases,
+	delta_deaths
+FROM bitemporal_agg;
+
+
+CREATE VIEW products.lateness AS
 SELECT
     bulletin_date,
     cast(sum(lateness_confirmed_and_probable_cases) AS DOUBLE PRECISION)
@@ -174,15 +241,15 @@ SELECT
     cast(sum(lateness_deaths) AS DOUBLE PRECISION)
         / nullif(sum(delta_deaths), 0)
         AS lateness_deaths
-FROM bitemporal_analysis
+FROM bitemporal_agg
 GROUP BY bulletin_date;
 
-COMMENT ON VIEW lateness_analysis IS
+COMMENT ON VIEW products.lateness IS
 'An estimate of how late on average new data for each bulletin
-is, based on the `bitemporal_analysis` view.';
+is, based on the `bitemporal_agg` view.';
 
 
-CREATE VIEW doubling_time AS
+CREATE VIEW doubling_times AS
 SELECT
     bulletin_date,
     datum_date,
@@ -200,12 +267,12 @@ SELECT
     CAST(window_size.days AS NUMERIC)
     	/ NULLIF(log2(cumulative_deaths) - log2(LAG(cumulative_deaths, window_size.days) OVER datum), 0)
     	AS cumulative_deaths
-FROM bitemporal_analysis
+FROM bitemporal_agg
 CROSS JOIN (VALUES (7), (14), (21)) AS window_size (days)
 WINDOW datum AS (
 	PARTITION BY bulletin_date, window_size.days
 	ORDER BY datum_date);
 
-COMMENT ON VIEW doubling_time IS
+COMMENT ON VIEW doubling_times IS
 'How long it took values to double, expressed in fractional days.
 Computed over windows of 7, 14 and 21 days.';
