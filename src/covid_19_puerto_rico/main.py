cd9ab3654b39b0e8d39f0163cb17090e3596f399
sacundim <anybody@sacundim.org>
2020-05-03 19:03:22 -0700

    Altair demo, which adds a lateness chart.

--- src/covid_19_puerto_rico/main.py
+++ src/covid_19_puerto_rico/main.py
@@ -1,13 +1,12 @@
 #/usr/bin/env/python3
 
+import altair as alt
 import argparse
 import datetime
 import logging
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-import seaborn as sns
 import sqlalchemy
+from psycopg2._psycopg import connection
 from sqlalchemy.sql import select
 
 def process_arguments():
@@ -21,17 +20,34 @@ def process_arguments():
 def main():
     logging.basicConfig(format='%(asctime)s %(message)s',
                         level=logging.INFO)
-    sns.set(style="darkgrid")
-
     args = process_arguments();
+    logging.info("bulletin-date is %s; output-dir is %s",
+                 args.bulletin_date, args.output_dir)
 
     engine = create_db()
     with engine.connect() as connection:
         main_graph(connection, args)
+        lateness_graph(connection, args)
 
 def main_graph(connection, args):
+    df = main_graph_data(connection, args)
+    lines = alt.Chart(df).mark_line(point=True).encode(
+        x='datum_date:T',
+        y=alt.X('value', scale=alt.Scale(type='log')),
+        color='variable',
+        tooltip=['datum_date', 'variable', 'value']
+    ).properties(
+        width=1024,
+        height=768
+    )
+    filename = f"{args.output_dir}/main_{args.bulletin_date}.html"
+    logging.info("Writing graph to %s", filename)
+    lines.save(filename)
+
+def main_graph_data(connection, args):
     meta = sqlalchemy.MetaData()
-    table = sqlalchemy.Table('main_graph', meta, autoload=True, autoload_with=connection)
+    table = sqlalchemy.Table('main_graph', meta, schema='products',
+                             autoload_with=connection)
     query = select([table.c.datum_date,
                     table.c.confirmed_cases,
                     table.c.probable_cases,
@@ -40,17 +56,42 @@ def main_graph(connection, args):
                     table.c.deaths,
                     table.c.announced_deaths]).where(table.c.bulletin_date == args.bulletin_date)
     df = pd.read_sql_query(query, connection)
-    logging.info("%s", df['positive_results'])
+    return adjust_frame(df, "datum_date")
 
-    graph = sns.relplot(x='datum_date', y='value', kind='line', hue='variable',
-                        data=pd.melt(df, ['datum_date']))
-    graph.fig.autofmt_xdate()
-    graph.set(yscale='log')
-
-    filename = f"{args.output_dir}/main_{args.bulletin_date}.png"
+def lateness_graph(connection, args):
+    df = lateness_data(connection, args)
+    bars = alt.Chart(df).mark_bar().encode(
+        x='value',
+        y='variable',
+        color='variable'
+    ).properties(
+        width=640,
+    ).facet(
+        row="bulletin_date"
+    )
+    filename = f"{args.output_dir}/lateness_{args.bulletin_date}.html"
     logging.info("Writing graph to %s", filename)
-    graph.savefig(filename, dpi=150)
+    bars.save(filename)
 
+def lateness_data(connection, args):
+    meta = sqlalchemy.MetaData()
+    table = sqlalchemy.Table('lateness', meta, schema='products',
+                             autoload_with=connection)
+    query = select([table.c.bulletin_date,
+                    table.c.confirmed_and_probable_cases,
+                    table.c.confirmed_cases,
+                    table.c.probable_cases,
+                    table.c.deaths]
+    ).where(
+        table.c.bulletin_date <= args.bulletin_date
+    )
+    df = pd.read_sql_query(query, connection)
+    return adjust_frame(df, "bulletin_date")
+
+
+def adjust_frame(df, date_column):
+    df[date_column] = pd.to_datetime(df[date_column])
+    return pd.melt(df, date_column)
 
 def create_db():
     url = sqlalchemy.engine.url.URL(
