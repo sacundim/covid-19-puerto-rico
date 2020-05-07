2d9e0d98d17557a20c4ed92c10fe579c122fb70c
Luis Casillas <luis@casillas.org>
2020-05-07 09:42:13 -0700

    The death_lag_animation chart now generates an actual animated gif

--- src/covid_19_puerto_rico/death_lag_animation.py
+++ src/covid_19_puerto_rico/death_lag_animation.py
@@ -4,25 +4,36 @@ from sqlalchemy.sql import text
 import altair as alt
 import logging
 import pandas as pd
+from pathlib import Path
+from wand.image import Image
 from .util import *
 
 
 def death_lag_animation(connection, args):
-    for current_date in pd.date_range(args.earliest_bulletin_date, args.bulletin_date):
-        df = death_lag_animation_data(connection,
-                                      args.earliest_bulletin_date,
-                                      current_date)
-        logging.info("death_lag_animation frame: %s", describe_frame(df))
+    Path(f"{args.output_dir}/death_lag_animation").mkdir(parents=True, exist_ok=True)
+    with Image() as gif:
+        for current_date in pd.date_range(args.earliest_bulletin_date, args.bulletin_date):
+            df = death_lag_animation_data(connection,
+                                          args.earliest_bulletin_date,
+                                          current_date)
+            logging.info("death_lag_animation frame: %s", describe_frame(df))
 
-        basename = f"{args.output_dir}/death_lag_animation_{current_date}"
-        save_chart(death_lag_animation_chart(df, args), basename, args.output_formats)
+            basename = f"{args.output_dir}/death_lag_animation/frame_{current_date.date()}"
+            save_chart(death_lag_animation_chart(df, args), basename, ['png'])
+            with Image(filename=f'{basename}.png') as frame:
+                gif.sequence.append(frame)
+        for frame in gif.sequence:
+            frame.delay = 120
+        gif.type = 'optimize'
+        gif.save(filename=f"{args.output_dir}/death_lag_animation_{args.bulletin_date}.gif")
+
 
 def death_lag_animation_chart(df, args):
     return alt.Chart(df).mark_line(point=True).encode(
         x=alt.X('datum_date', title='Fecha',
                 scale=alt.Scale(domain=(pd.to_datetime(args.earliest_bulletin_date),
                                         pd.to_datetime(args.bulletin_date)))),
-        y=alt.Y('value', title=None, scale=alt.Scale(domain=(80, 100))),
+        y=alt.Y('value', title=None, scale=alt.Scale(domain=(80, 110))),
         color=alt.Color('variable', title=None,
                         legend=alt.Legend(orient="top", labelLimit=250)),
     ).properties(
@@ -32,7 +43,6 @@ def death_lag_animation_chart(df, args):
 def death_lag_animation_data(connection, earliest_bulletin_date, bulletin_date):
     query = text('''
         SELECT 
---            bulletin_date,
             datum_date,
             COALESCE(deaths, 
                     MAX(deaths) OVER (PARTITION BY bulletin_date ROWS UNBOUNDED PRECEDING)) deaths,
