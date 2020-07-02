6a6d286ea670da40d79c40f3534a84798cba0214
Luis Casillas <luis@casillas.org>
2020-07-02 01:52:22 -0700

    CumulativeTestsVsCases chart

--- src/covid_19_puerto_rico/__init__.py
+++ src/covid_19_puerto_rico/__init__.py
@@ -45,6 +45,7 @@ def main():
         output_formats = frozenset(['json'])
 
     targets = [
+        molecular.CumulativeTestsVsCases(engine, args.output_dir, output_formats),
         charts.BulletinChartMismatch(engine, args.output_dir, output_formats),
         charts.ConsecutiveBulletinMismatch(engine, args.output_dir, output_formats),
         molecular.NewPositiveRate(engine, args.output_dir, output_formats),
