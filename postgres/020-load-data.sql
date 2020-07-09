9581ceef2d825af6478769aa6e9aca4be43553e3
sacundim <anybody@sacundim.org>
2020-07-09 16:44:10 -0700

    PRDoH put out some historical hospitalizations data, so here's a CSV and a chart.

--- postgres/020-load-data.sql
+++ postgres/020-load-data.sql
@@ -22,6 +22,10 @@ COPY bioportal_bitemporal
 FROM '/data/PuertoRico-bioportal-bitemporal.csv'
     CSV ENCODING 'UTF-8' HEADER NULL '';
 
+COPY hospitalizations
+FROM '/data/PuertoRico-hospitalizations.csv'
+    CSV ENCODING 'UTF-8' HEADER NULL '';
+
 COPY prpht_molecular_raw
 FROM '/data/PRPHT-molecular.csv'
     CSV ENCODING 'UTF-8' HEADER NULL '';
