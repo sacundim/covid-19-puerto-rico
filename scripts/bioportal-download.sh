185835d10b7f7a2b7975b2e67001ca9d95a1e655
sacundim <anybody@sacundim.org>
2020-07-29 10:00:30 -0700

    Automate the Bioportal download and conversion a bit more.

--- scripts/bioportal-download.sh
+++ scripts/bioportal-download.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
+set -e
+
 ENDPOINT="https://bioportal.salud.gov.pr/api/administration/reports/minimal-info-unique-tests"
 timestamp="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
-filename="minimal-info-unique-tests_${timestamp}.json"
+basename="minimal-info-unique-tests_${timestamp}"
 
-time wget --output-document="${filename}" "${ENDPOINT}"
+HERE="$(dirname $0)"
+TMP="$(dirname $0)/../tmp"
+
+time wget \
+    --output-document="${TMP}/${basename}.json" \
+    "${ENDPOINT}"
+
+echo "Converting to csv..."
+"${HERE}"/bioportal-json-to-csv.sh \
+    "${TMP}/${basename}.json" \
+    > "${TMP}/${basename}.csv"
+
+echo "Wrote output to ${TMP}/${basename}.csv"
