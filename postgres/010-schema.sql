7ebcec2378a06cac725800691a0be5a1f1bc0be6
sacundim <anybody@sacundim.org>
2020-09-21 01:42:15 -0700

    Much improved LatenessTiers chart.

--- postgres/010-schema.sql
+++ postgres/010-schema.sql
@@ -661,6 +661,25 @@ INNER JOIN canonical_municipal_names cmn
 ORDER BY municipality, bulletin_date;
 
 
+CREATE VIEW products.lateness_tiers AS
+SELECT
+	bulletin_date,
+	ranges.tier,
+	ranges.lo AS tier_order,
+	COALESCE(sum(delta_confirmed_cases) FILTER (
+		WHERE delta_confirmed_cases > 0
+	), 0) AS count
+FROM bitemporal_agg ba
+INNER JOIN (VALUES (0, 3, '0-3'),
+				   (4, 7, '4-7'),
+				   (8, 14, '8-14'),
+				   (14, NULL, '> 14')) AS ranges (lo, hi, tier)
+	ON ranges.lo <= age AND age <= COALESCE(ranges.hi, 2147483647)
+WHERE bulletin_date > '2020-04-24'
+GROUP BY bulletin_date, ranges.lo, ranges.hi, ranges.tier
+ORDER BY bulletin_date DESC, ranges.lo ASC;
+
+
 CREATE VIEW products.lateness_tiers_smoothed AS
 WITH min_date AS (
 	SELECT min(bulletin_date) min_bulletin_date
