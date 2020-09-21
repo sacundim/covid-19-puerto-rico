e838243fab443e3b9a684614857680b6c64ed641
Luis Casillas <luis@casillas.org>
2020-09-21 01:42:15 -0700

    Much improved LatenessTiers chart.

--- src/covid_19_puerto_rico/charts.py
+++ src/covid_19_puerto_rico/charts.py
@@ -917,36 +917,53 @@ class LatenessTiers(AbstractChart):
     SORT_ORDER = ['≤ 7 días', '≤ 14 días', '> 14 días']
 
     def fetch_data(self, connection):
-        table = sqlalchemy.Table('lateness_tiers_smoothed', self.metadata,
+        table = sqlalchemy.Table('lateness_tiers', self.metadata,
                                  schema='products', autoload=True)
         query = select([
             table.c.bulletin_date,
-            (table.c.count_one_week / 7.0).label('≤ 7 días'),
-            (table.c.count_two_week / 7.0).label('≤ 14 días'),
-            (table.c.count_useless / 7.0).label('> 14 días')
+            table.c.tier,
+            table.c.tier_order,
+            table.c.count
         ])
-        df = pd.read_sql_query(query, connection, parse_dates=['bulletin_date'])
-        return pd.melt(df, ['bulletin_date'])
+        return pd.read_sql_query(query, connection, parse_dates=['bulletin_date'])
 
     def filter_data(self, df, bulletin_date):
         return df.loc[df['bulletin_date'] <= pd.to_datetime(bulletin_date)]
 
     def make_chart(self, df, bulletin_date):
+        max_y = df.groupby(['bulletin_date'])['count'].sum()\
+            .rolling(7).mean().max()
+
         base = alt.Chart(df).transform_joinaggregate(
             groupby=['bulletin_date'],
-            total='sum(value)'
+            total='sum(count)'
+        ).transform_window(
+            groupby=['tier'],
+            sort=[{'field': 'bulletin_date'}],
+            frame=[-6, 0],
+            mean_count='mean(count)',
+            mean_total='mean(total)'
+        ).transform_calculate(
+            percent=alt.datum.count / alt.datum.total,
+            mean_percent=alt.datum.mean_count / alt.datum.mean_total
         ).mark_area(opacity=0.85).encode(
-            color=alt.Color('variable:N', title='Renglón', sort=self.SORT_ORDER,
-                            legend=alt.Legend(orient='top', titleOrient='left', offset=5)),
+            color=alt.Color('tier:N', title='Renglón (días)', sort=self.SORT_ORDER,
+                            legend=alt.Legend(orient='top', titleOrient='left', offset=5),
+                            scale=alt.Scale(scheme='redyellowgreen', reverse=True)),
+            order=alt.Order('tier_order:O'),
             tooltip=[alt.Tooltip('bulletin_date:T', title='Fecha de boletín'),
-                     alt.Tooltip('variable:N', title='Renglón'),
-                     alt.Tooltip('value:Q', format=".1f", title='Casos en renglón'),
-                     alt.Tooltip('total:Q', format=".1f", title='Casos total'),]
+                     alt.Tooltip('tier:N', title='Renglón'),
+                     alt.Tooltip('count:Q', format=",d", title='Casos en renglón (crudo)'),
+                     alt.Tooltip('total:Q', format=",d", title='Casos total (crudo)'),
+                     alt.Tooltip('mean_count:Q', format=".1f", title='Casos en renglón (promedio 7)'),
+                     alt.Tooltip('mean_total:Q', format=".1f", title='Casos total (promedio 7)'),
+                     alt.Tooltip('mean_percent:Q', format=".1%", title='% de total (promedio 7)')]
         )
 
         absolute = base.encode(
             x=alt.X('bulletin_date:T', title=None, axis=alt.Axis(ticks=False, labels=False)),
-            y=alt.Y('value:Q', title='Casos confirmados (promedio 7 días)',
+            y=alt.Y('mean_count:Q', title='Casos confirmados (promedio 7 días)',
+                    scale=alt.Scale(domain=[0, max_y]),
                     axis=alt.Axis(labelExpr="if(datum.value > 0, datum.label, '')")),
         ).properties(
             width=575, height=275
@@ -954,7 +971,7 @@ class LatenessTiers(AbstractChart):
 
         normalized = base.encode(
             x=alt.X('bulletin_date:T', title='Fecha de boletín'),
-            y=alt.Y('value:Q', stack='normalize', title='% renglón',
+            y=alt.Y('mean_count:Q', stack='normalize', title='% renglón',
                     axis=alt.Axis(format='%', labelExpr="if(datum.value < 1.0, datum.label, '')"))
         ).properties(
             width=575, height=75
