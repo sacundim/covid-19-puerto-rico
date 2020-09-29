f729db15756c116a0884f3f8c90988803550e525
Luis Casillas <luis@casillas.org>
2020-09-29 09:29:27 -0700

    Fix up S3 sync script:
    1. Preserve timestamp on file copy to avoid spurious uploads
    2. Sync the source_material directory regularly

--- scripts/aws-data-sync.sh
+++ scripts/aws-data-sync.sh
@@ -6,12 +6,18 @@ S3_DATA_URL="${S3_DATA_URL-s3://covid-19-puerto-rico-data}"
 HERE="$(dirname $0)"
 REPO_ROOT="${HERE}/.."
 BULLETIN_CASES_CSV="${REPO_ROOT}/assets/data/cases/PuertoRico-bitemporal.csv"
+SOURCE_MATERIAL_DIR="${REPO_ROOT}/assets/source_material"
 S3_SYNC_DIR="${REPO_ROOT}/s3-bucket-sync/covid-19-puerto-rico-data"
 
-echo "$(date): Syncing ${S3_SYNC_DIR} to ${S3_DATA_URL}"
-cp "${BULLETIN_CASES_CSV}" \
+
+echo "$(date): Copying ${BULLETIN_CASES_CSV} to ${S3_SYNC_DIR}/bulletin/cases/"
+cp -fp "${BULLETIN_CASES_CSV}" \
   "${S3_SYNC_DIR}/bulletin/cases/"
 
 echo "$(date): Syncing ${S3_SYNC_DIR} to ${S3_DATA_URL}"
 time aws s3 sync $* --exclude '*.DS_Store' \
   "${S3_SYNC_DIR}" "${S3_DATA_URL}"
+
+echo "$(date): Syncing ${SOURCE_MATERIAL_DIR} to ${S3_DATA_URL}/source_material"
+time aws s3 sync $* --exclude '*.DS_Store' \
+  "${SOURCE_MATERIAL_DIR}/" "${S3_DATA_URL}/source_material/"
