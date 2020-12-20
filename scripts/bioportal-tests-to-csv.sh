9fe66a35291e29dbc1a75e25141b65a7fe229a5e
Luis Casillas <luis@casillas.org>
2020-12-19 20:15:24 -0800

    The `patientCity` field in minimal-info-unique-tests got renamed to `city` back in August and I was too dumb to notice it

--- scripts/bioportal-tests-to-csv.sh
+++ scripts/bioportal-tests-to-csv.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+#
+# Convert a `minimal-info-unique-tests` download to CSV.
+#
 
 set -e
 set -o pipefail
@@ -6,8 +9,8 @@ set -o pipefail
 downloadedAt="${1:?"No downloadedAt argument given"}"
 file="${2:?"No argument file given"}"
 
-echo 'downloadedAt,collectedDate,reportedDate,ageRange,testType,result,patientCity,createdAt'
+echo 'downloadedAt,collectedDate,reportedDate,ageRange,testType,result,city,createdAt'
 cat "${file}" \
     | bunzip2 \
-    | jq -r '.[] | [.collectedDate, .reportedDate, .ageRange, .testType, .result, .patientCity, .createdAt] | @csv' \
+    | jq -r '.[] | [.collectedDate, .reportedDate, .ageRange, .testType, .result, .city, .createdAt] | @csv' \
     | sed -e "s/^/${downloadedAt},/"
