70a4fe0c5a4e4e11b41700daa177ffc7cb14ac75
Luis Casillas <luis@casillas.org>
2020-12-19 20:37:15 -0800

    Redo one-off Parquet conversion script for `minimal-info-unique-tests`. We're just doing the V3 files for now.

--- scripts/one-off/convert-tests.sh
+++ scripts/one-off/convert-tests.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 #
 # One-off script to convert old minimal-info-unique-tests_V* csv files to Parquet.
 # Uses this utility:
@@ -7,40 +6,27 @@
 # * https://github.com/cldellow/csv2parquet
 #
 
-set -e
-set -o pipefail
+set -eu -o pipefail
 
 HERE="$(dirname $0)"
 REPO_ROOT="${HERE}/../.."
 S3_SYNC_DIR="${REPO_ROOT}/s3-bucket-sync/covid-19-puerto-rico-data"
-TESTS_DIR="${S3_SYNC_DIR}/bioportal/tests"
+TESTS_DIR="${S3_SYNC_DIR}/bioportal/minimal-info-unique-tests"
 
-mkdir -p "${TESTS_DIR}"/parquet_v1
+mkdir -p "${TESTS_DIR}"/parquet_v3
 
-cd "${TESTS_DIR}/csv_v1"
-for file in minimal-info-unique-tests_*.csv.bz2
+for file in "${TESTS_DIR}"/json_v3/minimal-info-unique-tests_*.json.bz2
 do
   echo "$(date): converting ${file}..."
+  name="$(basename file)"
   bzcat "${file}" \
+    | "${HERE}"/../bioportal-tests-to-csv.sh \
     | time csv2parquet \
         --exclude patientid \
         --codec gzip \
         --row-group-size 10000000 \
         --output /dev/stdout \
         /dev/stdin \
-    > ../parquet_v1/"${file%.csv.bz2}".parquet
+    > "${TESTS_DIR}"/parquet_v3/"${name%.json.bz2}".parquet
 done
 
-cd ../csv_v2
-for file in minimal-info-unique-tests_*.csv.bz2
-do
-  echo "$(date): converting ${file}..."
-  bzcat "${file}" \
-    | time csv2parquet \
-        --exclude patientid \
-        --codec gzip \
-        --row-group-size 10000000 \
-        --output /dev/stdout \
-        /dev/stdin \
-    > ../parquet_v1/"${file%.csv.bz2}".parquet
-done
