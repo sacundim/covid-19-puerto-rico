862085ff00171a90dd38de489e668baf8eb2bea4
sacundim <anybody@sacundim.org>
2021-01-02 12:43:13 -0800

    Bump up CumulativeTestsVsCases axes

--- src/covid_19_puerto_rico/molecular.py
+++ src/covid_19_puerto_rico/molecular.py
@@ -300,7 +300,7 @@ class CumulativeTestsVsCases(AbstractMolecularChart):
         return df.loc[df['bulletin_date'] == effective_bulletin_date]
 
     def make_chart(self, df, bulletin_date):
-        max_x, max_y = 24_000, 400_000
+        max_x, max_y = 24_000, 450_000
 
         main = alt.Chart(df.dropna()).transform_calculate(
             tests_per_million=alt.datum.cumulative_tests / self.POPULATION_MILLIONS,
