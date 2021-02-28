9465d115bea8569e03a5025cb8ff1836e4e5cc86
sacundim <anybody@sacundim.org>
2021-02-27 23:25:20 -0800

    A view to parse types on `reported_hospital_utilization_timeseries`

--- aws/athena/create-hhs-schema.sql
+++ aws/athena/create-hhs-schema.sql
@@ -247,3 +247,59 @@ CREATE EXTERNAL TABLE covid_hhs_sources.reported_hospital_capacity_admissions_fa
 ) STORED AS PARQUET
 LOCATION 's3://covid-19-puerto-rico-data/HHS/reported_hospital_capacity_admissions_facility_level_weekly_average_timeseries/parquet/'
 ;
+
+
+-------------------------------------------------------------------------
+-------------------------------------------------------------------------
+--
+-- Minimal cleanup and type handling, plus filtering to Puerto Rico
+--
+
+CREATE OR REPLACE VIEW covid_hhs_sources.reported_hospital_utilization_timeseries_PR AS
+SELECT
+	date_parse(regexp_extract("$path", '202[012](\d{4})_(\d{4})'), '%Y%m%d_%H%i')
+		AS file_timestamp,
+	date(date) AS date,
+	CAST(NULLIF(inpatient_beds_used, '') AS INTEGER)
+		AS inpatient_beds_used,
+	CAST(NULLIF(inpatient_beds_used_coverage , '') AS INTEGER)
+		AS inpatient_beds_used_coverage,
+	CAST(NULLIF(inpatient_beds_used_covid, '') AS INTEGER)
+		AS inpatient_beds_used_covid,
+	CAST(NULLIF(inpatient_beds_used_covid_coverage, '') AS INTEGER)
+		AS inpatient_beds_used_covid_coverage,
+	CAST(NULLIF(previous_day_admission_adult_covid_confirmed, '') AS INTEGER)
+		AS previous_day_admission_adult_covid_confirmed,
+	CAST(NULLIF(previous_day_admission_adult_covid_confirmed_coverage, '') AS INTEGER)
+		AS previous_day_admission_adult_covid_confirmed_coverage,
+	CAST(NULLIF(previous_day_admission_adult_covid_suspected, '') AS INTEGER)
+		AS previous_day_admission_adult_covid_suspected,
+	CAST(NULLIF(previous_day_admission_adult_covid_suspected_coverage, '') AS INTEGER)
+		AS previous_day_admission_adult_covid_suspected_coverage,
+	CAST(NULLIF(previous_day_admission_pediatric_covid_confirmed, '') AS INTEGER)
+		AS previous_day_admission_pediatric_covid_confirmed,
+	CAST(NULLIF(previous_day_admission_pediatric_covid_confirmed_coverage, '') AS INTEGER)
+		AS previous_day_admission_pediatric_covid_confirmed_coverage,
+	CAST(NULLIF(previous_day_admission_pediatric_covid_suspected, '') AS INTEGER)
+		AS previous_day_admission_pediatric_covid_suspected,
+	CAST(NULLIF(previous_day_admission_pediatric_covid_suspected_coverage, '') AS INTEGER)
+		AS previous_day_admission_pediatric_covid_suspected_coverage,
+	CAST(NULLIF(staffed_adult_icu_bed_occupancy, '') AS INTEGER)
+		AS staffed_adult_icu_bed_occupancy,
+	CAST(NULLIF(staffed_adult_icu_bed_occupancy_coverage, '') AS INTEGER)
+		AS staffed_adult_icu_bed_occupancy_coverage,
+	CAST(NULLIF(staffed_icu_adult_patients_confirmed_and_suspected_covid, '') AS INTEGER)
+		AS staffed_icu_adult_patients_confirmed_and_suspected_covid,
+	CAST(NULLIF(staffed_icu_adult_patients_confirmed_and_suspected_covid_coverage, '') AS INTEGER)
+		AS staffed_icu_adult_patients_confirmed_and_suspected_covid_coverage,
+	CAST(NULLIF(staffed_icu_adult_patients_confirmed_covid, '') AS INTEGER)
+		AS staffed_icu_adult_patients_confirmed_covid,
+	CAST(NULLIF(staffed_icu_adult_patients_confirmed_covid_coverage, '') AS INTEGER)
+		AS staffed_icu_adult_patients_confirmed_covid_coverage,
+	CAST(NULLIF(total_staffed_adult_icu_beds, '') AS INTEGER)
+		AS total_staffed_adult_icu_beds,
+	CAST(NULLIF(total_staffed_adult_icu_beds_coverage, '') AS INTEGER)
+		AS total_staffed_adult_icu_beds_coverage
+FROM covid_hhs_sources.reported_hospital_utilization_timeseries
+WHERE state = 'PR'
+ORDER BY "$path" DESC, date DESC;
