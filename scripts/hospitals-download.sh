e33de582f5136bc2c46a2eba1594252fb0868c5b
sacundim <anybody@sacundim.org>
2021-02-28 18:24:08 -0800

    Kill off the state hospital capacity data from Postgres, since we're doing it in Athena. We still keep the facility level data in Postgres because the views we've built against that one are a pain to port to Athena.

--- scripts/hospitals-download.sh
+++ scripts/hospitals-download.sh
@@ -3,7 +3,6 @@
 # Download the current data sets from here and extract Puerto Rico:
 #
 # * https://healthdata.gov/dataset/covid-19-reported-patient-impact-and-hospital-capacity-facility
-# * https://healthdata.gov/dataset/covid-19-reported-patient-impact-and-hospital-capacity-state-timeseries
 #
 set -eu -o pipefail
 
@@ -32,21 +31,5 @@ wget --compress=gzip -O - "${CSV_URL}" \
   | xsv search --select state '^PR$' \
   > "${TMP}"/reported_hospital_capacity_admissions_facility_level_weekly_average_timeseries-PuertoRico.csv
 
-
-##################################################################
-##################################################################
-#
-# State time series data set
-#
-
-STATE_JSON_URL="https://healthdata.gov/api/3/action/package_show?id=83b4a668-9321-4d8c-bc4f-2bef66c49050&page=0"
-CSV_URL="$(wget -O - "${STATE_JSON_URL}" |jq -r '.result[0].resources[0].url')"
-
-wget --compress=gzip -O - "${CSV_URL}" \
-  | xsv search --select state '^PR$' \
-  > "${TMP}"/reported_hospital_utilization_timeseries-PuertoRico.csv
-
-
 mv "${TMP}"/reported_hospital_capacity_admissions_facility_level_weekly_average_timeseries-PuertoRico.csv \
-  "${TMP}"/reported_hospital_utilization_timeseries-PuertoRico.csv \
   "${REPO_ROOT}"/assets/data/HHS/
