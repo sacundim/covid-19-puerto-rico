973b5b850e37255bc83b760f19ca2cd66f693631
sacundim <anybody@sacundim.org>
2021-03-24 11:03:30 -0700

    I didn't understand the municipalities vaccination API at first, but now I've got it—you have to hit one URL per municipality.

--- src/covid_19_puerto_rico_downloader/covid19datos.py
+++ src/covid_19_puerto_rico_downloader/covid19datos.py
@@ -19,43 +19,167 @@ def process_arguments():
                         help='Directory to which to deposit the output files for sync')
     return parser.parse_args()
 
-URLS = {
-    'casos': 'https://covid19datos.salud.gov.pr/estadisticas/casos',
-    'defunc': 'https://covid19datos.salud.gov.pr/estadisticas/defunc',
-    'vacunaciones': 'https://covid19datos.salud.gov.pr/estadisticas/vacunaciones',
-    'hospitales': 'https://covid19datos.salud.gov.pr/estadisticas/hospitales',
-    'vacunaciones-municipio': 'https://covid19datos.salud.gov.pr/estadisticas/vacunaciones/municipio'
-}
-
 def covid19datos():
     """Entry point for PRDoH dashboard download code."""
     logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
     args = process_arguments()
+    Covid19Datos(args).run()
 
-    now = datetime.now(tz=timezone('America/Puerto_Rico'))
-    logging.info('Now = %s', now.isoformat())
 
-    s3_sync_dir = pathlib.Path(args.s3_sync_dir)
-    s3_sync_dir.mkdir(exist_ok=True)
-    dash_sync_dir = pathlib.Path(f'{s3_sync_dir}/covid19datos.salud.gov.pr')
-    dash_sync_dir.mkdir(exist_ok=True)
+class Covid19Datos():
+    """Downloader for Puerto Rico Department of Health dashboard"""
+    def __init__(self, args):
+        self.args = args
+        self.s3_sync_dir = pathlib.Path(args.s3_sync_dir)
+        self.dash_sync_dir = pathlib.Path(f'{self.s3_sync_dir}/covid19datos.salud.gov.pr')
 
-    for key, url in URLS.items():
-        jsonfile = download(key, url, now)
-        destination = pathlib.Path(f'{dash_sync_dir}/{key}')
+    URLS = {
+        'casos': 'https://covid19datos.salud.gov.pr/estadisticas/casos',
+        'defunc': 'https://covid19datos.salud.gov.pr/estadisticas/defunc',
+        'vacunaciones': 'https://covid19datos.salud.gov.pr/estadisticas/vacunaciones',
+        'hospitales': 'https://covid19datos.salud.gov.pr/estadisticas/hospitales'
+    }
+
+    MUNICIPIOS_URL = 'https://covid19datos.salud.gov.pr/estadisticas/vacunaciones/municipio'
+
+
+    def run(self):
+        self.make_directory_structure()
+        now = datetime.now(tz=timezone('America/Puerto_Rico'))
+        logging.info('Now = %s', now.isoformat())
+        self.download_urls(now)
+        self.download_municipios(now)
+        logging.info('Downloads all done!')
+
+    def make_directory_structure(self):
+        """Ensure all of the required directories exist."""
+        self.s3_sync_dir.mkdir(exist_ok=True)
+        self.dash_sync_dir.mkdir(exist_ok=True)
+
+    def make_destination_dir(self, key):
+        destination = pathlib.Path(f'{self.dash_sync_dir}/{key}')
         destination.mkdir(exist_ok=True)
-
-        logging.info('Moving %s to %s/', jsonfile, destination)
-        shutil.move(jsonfile, f'{destination}/{jsonfile}')
-
-    logging.info('Downloads all done!')
+        return destination
 
 
-def download(key, url, now):
-    logging.info('Downloading %s from %s', key, url)
-    r = requests.post(url)
-    outpath = f'{key}_{now.isoformat()}.json'
-    with open(outpath, 'wb') as fd:
-        for chunk in r.iter_content(chunk_size=128):
-            fd.write(chunk)
-    return outpath
+    def download_urls(self, now):
+        for key, url in self.URLS.items():
+            jsonfile = self.download_url(key, url, now)
+            destination = self.make_destination_dir(key)
+            logging.info('Moving %s to %s/', jsonfile, destination)
+            shutil.move(jsonfile, f'{destination}/{jsonfile}')
+
+    def download_url(self, key, url, now):
+        logging.info('Downloading %s from %s', key, url)
+        r = requests.post(url)
+        outpath = f'{key}_{now.isoformat()}.json'
+        with open(outpath, 'wb') as fd:
+            for chunk in r.iter_content(chunk_size=128):
+                fd.write(chunk)
+        return outpath
+
+
+    def download_municipios(self, now):
+        logging.info('Downloading municipios vaccination files...')
+        jsonfiles = []
+        for municipio in MUNICIPIOS:
+            jsonfiles.append(self.download_municipio(municipio, now))
+
+        destination = self.make_destination_dir('vacunaciones-municipios')
+        logging.info('Moving files to %s/', destination)
+        for jsonfile in jsonfiles:
+            shutil.move(jsonfile, f'{destination}/{jsonfile}')
+
+    def download_municipio(self, municipio, now):
+        url = f'{self.MUNICIPIOS_URL}/{municipio}'
+        logging.info('Downloading %s from %s', municipio, url)
+        r = requests.post(url)
+        outpath = f'vacunaciones-{municipio}_{now.isoformat()}.json'
+        with open(outpath, 'wb') as fd:
+            for chunk in r.iter_content(chunk_size=128):
+                fd.write(chunk)
+        return outpath
+
+
+MUNICIPIOS = [
+    'Adjuntas',
+    'Aguada',
+    'Aguadilla',
+    'Aguas Buenas',
+    'Aibonito',
+    'Añasco',
+    'Arecibo',
+    'Arroyo',
+    'Barceloneta',
+    'Barranquitas',
+    'Bayamón',
+    'Cabo Rojo',
+    'Caguas',
+    'Camuy',
+    'Canóvanas',
+    'Carolina',
+    'Cataño',
+    'Cayey',
+    'Ceiba',
+    'Ciales',
+    'Cidra',
+    'Coamo',
+    'Comerío',
+    'Corozal',
+    'Culebra',
+    'Dorado',
+    'Fajardo',
+    'Florida',
+    'Guánica',
+    'Guayama',
+    'Guayanilla',
+    'Guaynabo',
+    'Gurabo',
+    'Hatillo',
+    'Hormigueros',
+    'Humacao',
+    'Isabela',
+    'Jayuya',
+    'Juana Díaz',
+    'Juncos',
+    'Lajas',
+    'Lares',
+    'Las Marías',
+    'Las Piedras',
+    'Loíza',
+    'Luquillo',
+    'Manatí',
+    'Maricao',
+    'Maunabo',
+    'Mayagüez',
+    'Moca',
+    'Morovis',
+    'Naguabo',
+    'Naranjito',
+    'Orocovis',
+    'Patillas',
+    'Peñuelas',
+    'Ponce',
+    'Quebradillas',
+    'Rincón',
+    # This one doesn't have the accent on the "í" in the website,
+    # but luckily the API endpoint is accent-insensitive
+    'Río Grande',
+    'Sabana Grande',
+    'Salinas',
+    'San Germán',
+    'San Juan',
+    'San Lorenzo',
+    'San Sebastián',
+    'Santa Isabel',
+    'Toa Alta',
+    'Toa Baja',
+    'Trujillo Alto',
+    'Utuado',
+    'Vega Alta',
+    'Vega Baja',
+    'Vieques',
+    'Villalba',
+    'Yabucoa',
+    'Yauco'
+]
