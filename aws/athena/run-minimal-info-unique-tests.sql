0ae038ad662f77b7f7552bd0efff2b3195b4f518
sacundim <anybody@sacundim.org>
2021-04-18 21:58:27 -0700

    Improvements to minimal-info-unique-tests municipal analysis

--- aws/athena/run-minimal-info-unique-tests.sql
+++ aws/athena/run-minimal-info-unique-tests.sql
@@ -2,10 +2,15 @@
 -- The `minimal-info-unique-tests` row-per-test dataset.
 -- We only run this occasionally for ad-hoc analyses.
 --
+
 MSCK REPAIR TABLE covid_pr_sources.minimal_info_unique_tests_parquet_v4;
-DROP TABLE IF EXISTS covid_pr_etl.bioportal_tests;
-CREATE TABLE covid_pr_etl.bioportal_tests WITH (
-    format = 'PARQUET'
+
+DROP TABLE IF EXISTS covid_pr_etl.minimal_info_unique_tests;
+
+CREATE TABLE covid_pr_etl.minimal_info_unique_tests WITH (
+    format = 'PARQUET',
+    bucketed_by = ARRAY['bulletin_date'],
+    bucket_count = 4
 ) AS
 WITH first_clean AS (
     SELECT
@@ -43,7 +48,7 @@ WITH first_clean AS (
         COALESCE(result, '') LIKE '%Positive%' AS positive
     FROM covid_pr_sources.minimal_info_unique_tests_parquet_v4
     -- IMPORTANT: This prunes partitions
-    WHERE downloaded_date >= cast(date_add('day', -31, current_date) AS VARCHAR)
+    WHERE downloaded_date >= cast(date_add('day', -32, current_date) AS VARCHAR)
 )
 SELECT
     *,
@@ -92,3 +97,117 @@ SELECT
 	    ELSE coalesce(raw_reported_date, raw_collected_date, downloaded_date)
     END AS reported_date
 FROM first_clean;
+
+
+DROP TABLE IF EXISTS covid_pr_etl.municipal_tests_cube;
+
+CREATE TABLE covid_pr_etl.municipal_tests_cube WITH (
+    format = 'PARQUET',
+    bucketed_by = ARRAY['downloaded_at'],
+    bucket_count = 1
+) AS
+SELECT
+    downloaded_at,
+    bulletin_date,
+    collected_date,
+    reported_date,
+    municipality,
+    test_type,
+    count(*) AS specimens,
+    count(*) FILTER (WHERE positive) AS positives
+FROM covid_pr_etl.minimal_info_unique_tests
+WHERE DATE '2020-03-01' <= collected_date
+AND collected_date <= bulletin_date
+AND DATE '2020-03-01' <= reported_date
+AND reported_date <= bulletin_date
+GROUP BY
+    downloaded_at,
+    bulletin_date,
+    collected_date,
+    reported_date,
+    municipality,
+    test_type;
+
+
+DROP TABLE IF EXISTS covid_pr_etl.municipal_tests_collected_agg;
+
+CREATE TABLE covid_pr_etl.municipal_tests_collected_agg WITH (
+    format = 'PARQUET',
+    bucketed_by = ARRAY['downloaded_at'],
+    bucket_count = 1
+) AS
+SELECT
+    downloaded_at,
+    bulletin_date,
+    collected_date,
+    municipality,
+    test_type,
+    sum(specimens) AS specimens,
+    sum(positives) AS positives,
+    sum(sum(specimens)) OVER (
+        PARTITION BY downloaded_at, bulletin_date, municipality, test_type
+        ORDER BY collected_date
+    ) AS cumulative_specimens,
+    sum(sum(positives)) OVER (
+        PARTITION BY downloaded_at, bulletin_date, municipality, test_type
+        ORDER BY collected_date
+    ) AS cumulative_positives,
+    sum(specimens) - coalesce(lag(sum(specimens), 1, 0) OVER (
+        PARTITION BY downloaded_at, bulletin_date, municipality, test_type
+        ORDER BY collected_date
+    ), 0) AS delta_specimens,
+    sum(positives) - coalesce(lag(sum(positives), 1, 0) OVER (
+            PARTITION BY downloaded_at, bulletin_date, municipality, test_type
+            ORDER BY collected_date
+        ), 0) AS delta_positives
+FROM covid_pr_etl.municipal_tests_cube
+GROUP BY
+    downloaded_at,
+    bulletin_date,
+    collected_date,
+    municipality,
+    test_type;
+
+
+-------------------------------------------------------------------------
+-------------------------------------------------------------------------
+--
+-- Views
+--
+
+CREATE OR REPLACE VIEW covid_pr_etl.municipal_tests_per_capita AS
+SELECT
+    downloaded_at,
+    bulletin_date,
+    collected_date,
+    municipality,
+    population,
+    sum(specimens) AS specimens,
+    coalesce(sum(specimens) FILTER (
+        WHERE test_type = 'Antígeno'
+    ), 0) AS antigens,
+    coalesce(sum(specimens) FILTER (
+        WHERE test_type = 'Molecular'
+    ), 0) AS molecular,
+    sum(cumulative_specimens) AS cumulative_specimens,
+    coalesce(sum(cumulative_specimens) FILTER (
+        WHERE test_type = 'Antígeno'
+    ), 0) AS cumulative_antigens,
+    coalesce(sum(cumulative_specimens) FILTER (
+        WHERE test_type = 'Molecular'
+    ), 0) AS cumulative_molecular
+FROM covid_pr_etl.municipal_tests_collected_agg tests
+INNER JOIN covid_pr_sources.acs_2019_5y_municipal_race
+    USING (municipality)
+WHERE test_type IN ('Antígeno', 'Molecular')
+GROUP BY
+    downloaded_at,
+    bulletin_date,
+    municipality,
+    collected_date,
+    population
+ORDER BY
+    downloaded_at,
+    bulletin_date,
+    municipality,
+    collected_date;
