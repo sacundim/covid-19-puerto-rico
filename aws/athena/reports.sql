558c92805fc479b3e5f4f3bc24690ca791fc69e1
sacundim <anybody@sacundim.org>
2021-04-27 21:47:33 -0700

    Add sum of tests to `reports.sql` query

--- aws/athena/reports.sql
+++ aws/athena/reports.sql
@@ -37,18 +37,22 @@ WITH bulletins AS (
 SELECT
 	max_bulletin_date "Datos",
 	bio.collected_date AS "Muestras",
-	bio.molecular "Moleculares",
+	bio.encounters "Evaluados",
+	(bio.cumulative_encounters - lag(bio.cumulative_encounters, 7) OVER (
+		ORDER BY bio.collected_date
+	)) / 7.0 AS "Promedio",
+	bio.molecular "Molecular",
 	(bio.cumulative_molecular - lag(bio.cumulative_molecular, 7) OVER (
 		ORDER BY bio.collected_date
-	)) / 7.0 AS "Promedio 7 días",
+	)) / 7.0 AS "Promedio",
 	bio.antigens "Antígeno",
 	(bio.cumulative_antigens - lag(bio.cumulative_antigens, 7) OVER (
 		ORDER BY bio.collected_date
-	)) / 7.0 AS "Promedio 7 días",
-	bio.cases "Casos (Bioportal)",
+	)) / 7.0 AS "Promedio",
+	bio.cases "Casos",
 	(bio.cumulative_cases - lag(bio.cumulative_cases, 7) OVER (
 		ORDER BY bio.collected_date
-	)) / 7.0 AS "Promedio 7 días"
+	)) / 7.0 AS "Promedio"
 FROM covid_pr_etl.bioportal_encounters_agg bio
 INNER JOIN bulletins
 	ON bulletins.max_bulletin_date = bio.bulletin_date
