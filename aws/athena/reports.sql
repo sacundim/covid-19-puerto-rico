b5630852859bf73ae3c4b9bd7a73626292e0241d
sacundim <anybody@sacundim.org>
2021-07-14 00:22:34 -0700

    Add a doubling times ad-hoc query

--- aws/athena/reports.sql
+++ aws/athena/reports.sql
@@ -290,3 +290,51 @@ SELECT
 	tiberius_1dosis,
 	tiberius_2dosis
 FROM covid19datos_sources.vacunaciones_flat;
+
+
+--
+-- Doubling times, using 7- and 14-day windows
+--
+WITH bulletins AS (
+	SELECT max(bulletin_date) AS max_bulletin_date
+	FROM covid_pr_etl.bioportal_encounters_agg
+)
+SELECT
+	bulletin_date,
+	collected_date,
+	(cumulative_cases - lag(cumulative_cases, 7) OVER (
+		PARTITION BY bulletin_date
+		ORDER BY collected_date
+	)) cases_7,
+	7 / (
+		log2(cumulative_cases - lag(cumulative_cases, 7) OVER (
+			PARTITION BY bulletin_date
+			ORDER BY collected_date
+		)) - log2(lag(cumulative_cases, 7) OVER (
+			PARTITION BY bulletin_date
+			ORDER BY collected_date
+		) - lag(cumulative_cases, 14) OVER (
+			PARTITION BY bulletin_date
+			ORDER BY collected_date
+		))
+	) doubling_days_7,
+	(cumulative_cases - lag(cumulative_cases, 14) OVER (
+		PARTITION BY bulletin_date
+		ORDER BY collected_date
+	)) cases_14,
+	14 / (
+		log2(cumulative_cases - lag(cumulative_cases, 14) OVER (
+			PARTITION BY bulletin_date
+			ORDER BY collected_date
+		)) - log2(lag(cumulative_cases, 14) OVER (
+			PARTITION BY bulletin_date
+			ORDER BY collected_date
+		) - lag(cumulative_cases, 28) OVER (
+			PARTITION BY bulletin_date
+			ORDER BY collected_date
+		))
+	) doubling_days_14
+FROM covid_pr_etl.bioportal_encounters_agg bio
+INNER JOIN bulletins
+	ON bulletins.max_bulletin_date = bio.bulletin_date
+ORDER BY bulletin_date, collected_date DESC;
