387c56fe0cc9bb47c8e0f8a6591c8fad31f20c70
sacundim <anybody@sacundim.org>
2021-07-24 12:34:41 -0700

    Recreation of some old Postgres-based views in Athena

--- aws/athena/run-covid19datos-v2-etl.sql
+++ aws/athena/run-covid19datos-v2-etl.sql
@@ -285,3 +285,44 @@ SELECT
         ), 0) AS delta_deaths
 FROM joined
 ORDER BY bulletin_date, datum_date;
+
+
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+--
+-- Views for diverse reports.
+--
+
+CREATE VIEW covid19datos_v2_etl.daily_deltas AS
+SELECT
+    bulletin_date,
+	datum_date,
+	delta_confirmed_cases,
+	delta_probable_cases,
+	delta_deaths
+FROM covid19datos_v2_etl.bulletin_cases
+-- We exclude the earliest bulletin date because it's artificially big
+WHERE bulletin_date > (
+	SELECT min(bulletin_date)
+	FROM covid19datos_v2_etl.bulletin_cases
+);
+
+
+CREATE VIEW covid19datos_v2_etl.lateness_tiers AS
+SELECT
+	bulletin_date,
+	ranges.tier,
+	ranges.lo AS tier_order,
+	COALESCE(sum(delta_confirmed_cases) FILTER (
+		WHERE delta_confirmed_cases > 0
+	), 0) AS count
+FROM covid19datos_v2_etl.bulletin_cases ba
+INNER JOIN (VALUES (0, 3, '0-3'),
+				   (4, 7, '4-7'),
+				   (8, 14, '8-14'),
+				   (14, NULL, '> 14')) AS ranges (lo, hi, tier)
+	ON ranges.lo <= age AND age <= COALESCE(ranges.hi, 2147483647)
+WHERE bulletin_date > DATE '2020-04-24'
+GROUP BY bulletin_date, ranges.lo, ranges.hi, ranges.tier
+ORDER BY bulletin_date DESC, ranges.lo ASC;
