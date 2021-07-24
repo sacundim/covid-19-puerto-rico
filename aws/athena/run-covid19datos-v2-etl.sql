4e633c335deeb7280824ed3fd02a93825ff40834
sacundim <anybody@sacundim.org>
2021-07-24 12:19:24 -0700

    Recreation of older ETL bulletin_cases table using Covid19Datos V2

--- aws/athena/run-covid19datos-v2-etl.sql
+++ aws/athena/run-covid19datos-v2-etl.sql
@@ -178,3 +178,110 @@ SELECT
 		AS fe_registro
 FROM covid19datos_v2_sources.pruebas_parquet
 ORDER BY downloaded_at, fe_prueba;
+
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+--
+-- Recreate the old bulletin_cases table that I used to make from the daily
+-- PDFs.  In fact, we're still pulling here from my old CSV.
+--
+
+CREATE TABLE covid19datos_v2_etl.bulletin_cases WITH (
+    format = 'PARQUET',
+    bucketed_by = ARRAY['bulletin_date'],
+    bucket_count = 1
+) AS
+WITH casos AS (
+	WITH downloads AS (
+		SELECT
+			downloaded_date,
+			max(downloaded_at) downloaded_at
+		FROM covid19datos_v2_etl.casos
+		GROUP BY downloaded_date
+	)
+	SELECT
+		date_add('day', -1, date(downloaded_at AT TIME ZONE 'America/Puerto_Rico'))
+			AS bulletin_date,
+		sample_date AS datum_date,
+		count(*) FILTER (WHERE class = 'CONFIRMADO')
+			AS confirmed_cases,
+		count(*) FILTER (WHERE class = 'PROBABLE')
+			AS probable_cases
+	FROM covid19datos_v2_etl.casos casos
+	INNER JOIN downloads
+		USING (downloaded_date, downloaded_at)
+	GROUP BY downloaded_at, sample_date
+), defunciones AS (
+	WITH downloads AS (
+		SELECT
+			downloaded_date,
+			max(downloaded_at) downloaded_at
+		FROM covid19datos_v2_etl.casos
+		GROUP BY downloaded_date
+	)
+	SELECT
+		date_add('day', -1, date(downloaded_at AT TIME ZONE 'America/Puerto_Rico'))
+			AS bulletin_date,
+		fe_muerte AS datum_date,
+		count(*) deaths
+	FROM covid19datos_v2_etl.defunciones defunciones
+	INNER JOIN downloads
+		USING (downloaded_date, downloaded_at)
+	GROUP BY downloaded_at, fe_muerte
+), joined AS (
+	SELECT
+		bulletin_date,
+		datum_date,
+		confirmed_cases,
+		probable_cases,
+		COALESCE(deaths, 0) deaths
+	FROM casos
+	FULL OUTER JOIN defunciones
+		USING (bulletin_date, datum_date)
+	UNION
+	-- Merge with the old hand-curated data set:
+    SELECT
+        from_iso8601_date(bulletin_date) AS bulletin_date,
+        from_iso8601_date(datum_date) AS datum_date,
+        CAST(nullif(confirmed_cases, '') AS INTEGER) AS confirmed_cases,
+        CAST(nullif(probable_cases, '') AS INTEGER) AS probable_cases,
+        CAST(nullif(deaths, '') AS INTEGER) AS deaths
+    FROM covid_pr_sources.bulletin_cases_csv
+)
+SELECT
+	bulletin_date,
+	datum_date,
+	date_diff('day', datum_date, bulletin_date)
+		AS age,
+	confirmed_cases,
+    sum(confirmed_cases) OVER (
+        PARTITION BY bulletin_date
+        ORDER BY datum_date
+    ) AS cumulative_confirmed_cases,
+    COALESCE(confirmed_cases, 0)
+        - COALESCE(lag(confirmed_cases) OVER (
+            PARTITION BY datum_date
+            ORDER BY bulletin_date
+        ), 0) AS delta_confirmed_cases,
+    sum(probable_cases) OVER (
+        PARTITION BY bulletin_date
+        ORDER BY datum_date
+    ) AS cumulative_probable_cases,
+    COALESCE(probable_cases, 0)
+        - COALESCE(lag(probable_cases) OVER (
+            PARTITION BY datum_date
+            ORDER BY bulletin_date
+        ), 0) AS delta_probable_cases,
+	COALESCE(deaths, 0) deaths,
+    sum(deaths) OVER (
+        PARTITION BY bulletin_date
+        ORDER BY datum_date
+    ) AS cumulative_deaths,
+    COALESCE(deaths, 0)
+        - COALESCE(lag(deaths) OVER (
+            PARTITION BY datum_date
+            ORDER BY bulletin_date
+        ), 0) AS delta_deaths
+FROM joined
+ORDER BY bulletin_date, datum_date;
