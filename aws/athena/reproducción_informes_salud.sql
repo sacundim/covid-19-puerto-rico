0f699d6e1d7c98da31245975d72e1e9e79d4c58c
sacundim <anybody@sacundim.org>
2021-09-25 10:01:29 -0700

    Fix mislabeling/misordering of ad-hoc queries

--- aws/athena/reproducción_informes_salud.sql
+++ aws/athena/reproducción_informes_salud.sql
@@ -1,25 +1,6 @@
 --
 -- Reproducir (más or menos) la carátula del viejo informe de casos
 --
-WITH bulletins AS (
-	SELECT max(bulletin_date) AS bulletin_date
-	FROM covid19datos_v2_etl.bulletin_cases
-)
-SELECT
-	bulletin_date "Fecha de actualización de datos",
-	datum_date "Fecha de muestra",
-	confirmed_cases "Casos confirmados",
-	cumulative_confirmed_cases "Acumulados",
-	probable_cases "Casos probables",
-	cumulative_probable_cases "Casos probables"
-FROM covid19datos_v2_etl.bulletin_cases
-INNER JOIN bulletins USING (bulletin_date)
-ORDER BY datum_date DESC;
-
-
---
--- Reproducir las tablas de casos confirmados y probables por fecha de muestra
---
 SELECT
 	bulletin_date "Fecha actualización de datos",
 	sum(delta_confirmed_cases) FILTER (
@@ -39,6 +20,25 @@ SELECT
 FROM covid19datos_v2_etl.bulletin_cases
 GROUP BY bulletin_date
 ORDER BY bulletin_date DESC;
+
+
+--
+-- Reproducir las tablas de casos confirmados y probables por fecha de muestra
+--
+WITH bulletins AS (
+	SELECT max(bulletin_date) AS bulletin_date
+	FROM covid19datos_v2_etl.bulletin_cases
+)
+SELECT
+	bulletin_date "Fecha de actualización de datos",
+	datum_date "Fecha de muestra",
+	confirmed_cases "Casos confirmados",
+	cumulative_confirmed_cases "Acumulados",
+	probable_cases "Casos probables",
+	cumulative_probable_cases "Casos probables"
+FROM covid19datos_v2_etl.bulletin_cases
+INNER JOIN bulletins USING (bulletin_date)
+ORDER BY datum_date DESC;
 
 
 --
