dc2f3a5e8129023bb81f43fd28ebd5def8f31f0a
sacundim <anybody@sacundim.org>
2021-12-08 20:34:54 -0800

    Fix Covid19Datos vs. Bioportal comparison query

--- aws/athena-dbt/analysis/popular/prdoh_vs_bioportal.sql
+++ aws/athena-dbt/analysis/popular/prdoh_vs_bioportal.sql
@@ -4,14 +4,14 @@
 --
 WITH bulletins AS (
 	SELECT max(bulletin_date) AS max_bulletin_date
-	FROM covid19_puerto_rico_model.bulletin_cases
+	FROM {{ ref('bulletin_cases') }}
 )
 SELECT
 	max_bulletin_date "Datos",
 	bio.collected_date AS "Muestras",
 	bul.confirmed_cases + COALESCE(bul.probable_cases, 0)
 		AS "Casos (oficial)",
-	((bul.cumulative_confirmed_cases + COALESCE(bul.cumulative_probable_cases , 0))
+	((bul.cumulative_confirmed_cases + COALESCE(bul.cumulative_probable_cases, 0))
 		- lag(bul.cumulative_confirmed_cases + COALESCE(bul.cumulative_probable_cases , 0), 7) OVER (
 		ORDER BY bio.collected_date
 	)) / 7.0 AS "Promedio (7 días)",
@@ -23,10 +23,10 @@ SELECT
 		- COALESCE(bul.confirmed_cases, 0)
 		- COALESCE(bul.probable_cases, 0)
 		AS "Exceso Bioportal"
-FROM covid19_puerto_rico_model.bioportal_encounters_agg bio
+FROM {{ ref('bioportal_encounters_agg') }} bio
 INNER JOIN bulletins
 	ON bulletins.max_bulletin_date = bio.bulletin_date
-LEFT OUTER JOIN covid19_puerto_rico_model.bulletin_cases bul
+LEFT OUTER JOIN {{ ref('bulletin_cases') }} bul
 	ON bul.bulletin_date = bio.bulletin_date
 	AND bul.datum_date = bio.collected_date
 ORDER BY bio.collected_date DESC;
