3cd5ad5263f79fcf6247f74cb6199137532c4ddf
sacundim <anybody@sacundim.org>
2021-12-29 09:22:53 -0800

    Update hospitalizations analysis query

--- aws/athena-dbt/analysis/popular/basic_hospitalizations_data.sql
+++ aws/athena-dbt/analysis/popular/basic_hospitalizations_data.sql
@@ -1,18 +1,20 @@
 --
 -- Datos básicos de hospitalizaciones
 --
+WITH bulletins AS (
+	SELECT max(bulletin_date) AS max_bulletin_date
+	FROM {{ ref('hospitalizations') }}
+)
 SELECT
-	file_timestamp AS "Datos",
+	bulletin_date AS "Datos",
 	date AS "Fecha",
-	inpatient_beds_used_covid AS "Camas ocupadas por COVID",
-	previous_day_admission_adult_covid_confirmed
-		+ previous_day_admission_adult_covid_suspected
-		+ previous_day_admission_pediatric_covid_confirmed
-		+ previous_day_admission_pediatric_covid_suspected
-		AS "Admisiones por COVID",
-	staffed_icu_adult_patients_confirmed_and_suspected_covid
-		AS "Camas UCI ocupadas por COVID",
-	staffed_adult_icu_bed_occupancy AS "Camas UCI ocupadas (cualquier causa)",
-	total_staffed_adult_icu_beds AS "Total camas UCI"
-FROM {{ ref('hhs_hospitals') }}
+	hospitalized_currently AS "Camas ocupadas por COVID",
+	previous_day_admission_adult_covid
+		+ previous_day_admission_pediatric_covid
+		AS "Ingresos por COVID",
+	in_icu_currently
+		AS "Camas UCI ocupadas por COVID"
+FROM {{ ref('hospitalizations') }} hosp
+INNER JOIN bulletins
+	ON bulletins.max_bulletin_date = hosp.bulletin_date
 ORDER BY date DESC;
