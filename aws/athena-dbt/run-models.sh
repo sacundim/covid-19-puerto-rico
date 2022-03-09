3e227ef5f958c1859784e0cc04653dc7151cd5f8
sacundim <anybody@sacundim.org>
2022-03-09 09:28:10 -0800

    Upgrade to DBT 1.0

--- aws/athena-dbt/run-models.sh
+++ aws/athena-dbt/run-models.sh
@@ -7,11 +7,7 @@ set -e -x
 
 dbt test --select 'source:*'
 
-# The DBT Athena third party plugin is janky, and the seed
-# fails when there's more than 1 thread.
-dbt seed --threads 1
-
-dbt run
+dbt seed && dbt run
 
 # TRICKY: We'd like to do this first but it depends on
 # refreshing table partitions (MSCK REPAIR TABLE):
