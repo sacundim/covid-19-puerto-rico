f8df273813b03ac58e47de7b729447e551f526f6
sacundim <anybody@sacundim.org>
2022-03-19 22:41:11 -0700

    Github Action to build and push Docker image

--- Terraform/IAM.tf
+++ Terraform/IAM.tf
@@ -23,3 +23,46 @@ resource "aws_iam_policy" "data_bucket_rw" {
   })
 }
 
+
+##############################################################################
+##############################################################################
+##
+## IAM setup for GitHub Actions to push Docker image to ECR
+##
+
+resource "aws_iam_user_policy" "ecr_push_user_policy" {
+  name = "covid-19-puerto-rico-downloader-ecr-push"
+  user = aws_iam_user.ecr_push_user.name
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "VisualEditor0",
+        "Effect": "Allow",
+        "Action": [
+          "ecr:CompleteLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:InitiateLayerUpload",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage"
+        ],
+        "Resource": aws_ecr_repository.downloader_repo.arn
+      },
+      {
+        "Sid": "VisualEditor1",
+        "Effect": "Allow",
+        "Action": "ecr:GetAuthorizationToken",
+        "Resource": "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user" "ecr_push_user" {
+  name = "covid-19-puerto-rico-downloader-github-ecr"
+  path = "/"
+  tags = {
+    Project = var.project_name
+  }
+}
