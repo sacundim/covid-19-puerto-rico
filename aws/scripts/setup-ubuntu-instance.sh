9021f52a2b73e1f078cb15d07ca34d067ebb8deb
sacundim <anybody@sacundim.org>
2022-03-26 23:15:35 -0700

    Improvements to util script for setting up a devel EC2 instance.

--- aws/scripts/setup-ubuntu-instance.sh
+++ aws/scripts/setup-ubuntu-instance.sh
@@ -2,9 +2,14 @@
 #
 # Install devel stuff on a fresh EC2 instance
 #
+# To log in: ssh ubuntu@[public-ec2-hostname]
+#
+
+# This repository has a big menu of Python versions
+sudo add-apt-repository ppa:deadsnakes/ppa
 
 sudo apt update
-sudo apt install -y python3-pip python3-venv awscli
+sudo apt install -y python3.9 awscli
 
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
 source $HOME/.poetry/env
