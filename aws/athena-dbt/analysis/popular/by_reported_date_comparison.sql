a1c06342be1b629c6f3d8ebf7d84247e18566102
sacundim <anybody@sacundim.org>
2022-04-21 02:07:08 -0700

    Analysis query to produce a report-date table with official data and my Bioportal analysis

---
+++ aws/athena-dbt/analysis/popular/by_reported_date_comparison.sql
@@ -0,0 +1,68 @@
+--
+-- Cases by reported date according to official data, vs.
+-- cases by reported date accoring to my analysis.
+--
+WITH official AS (
+	SELECT
+		bulletin_date,
+		sum(delta_confirmed_cases) + sum(delta_probable_cases)
+			AS delta_cases,
+		avg(sum(delta_confirmed_cases) + sum(delta_probable_cases)) OVER (
+			ORDER BY bulletin_date
+			ROWS 6 PRECEDING
+		) AS mean_delta_cases
+	FROM {{ ref('bulletin_cases') }}
+	GROUP BY bulletin_date
+), mine AS (
+	SELECT
+		bulletin_date,
+		sum(delta_cases) FILTER (
+			WHERE date_diff('day', collected_date, bulletin_date) < 14
+		) AS delta_cases,
+		avg(sum(delta_cases) FILTER (
+			WHERE date_diff('day', collected_date, bulletin_date) < 14
+		)) OVER (
+			ORDER BY bulletin_date
+			ROWS 6 PRECEDING
+		) AS mean_delta_cases,
+		sum(delta_first_infections) FILTER (
+			WHERE date_diff('day', collected_date, bulletin_date) < 14
+		) AS delta_first_infections,
+		avg(sum(delta_first_infections) FILTER (
+			WHERE date_diff('day', collected_date, bulletin_date) < 14
+		)) OVER (
+			ORDER BY bulletin_date
+			ROWS 6 PRECEDING
+		) AS mean_delta_first_infections,
+		sum(delta_possible_reinfections) FILTER (
+			WHERE date_diff('day', collected_date, bulletin_date) < 14
+		) AS delta_cases,
+		avg(sum(delta_possible_reinfections) FILTER (
+			WHERE date_diff('day', collected_date, bulletin_date) < 14
+		)) OVER (
+			ORDER BY bulletin_date
+			ROWS 6 PRECEDING
+		) AS mean_delta_possible_reinfections
+	FROM {{ ref('bioportal_encounters_agg') }}
+	GROUP BY bulletin_date
+)
+SELECT
+	bulletin_date "Report date",
+--	official.delta_cases "Oficial",
+	official.mean_delta_cases "Official",
+--	1e5 * official.mean_delta_cases / 3.411e6 "Por 100k (NYT)",
+	100.0 * (official.mean_delta_cases / lag(official.mean_delta_cases, 14) OVER (
+		ORDER BY bulletin_date
+	) - 1) AS "14-day change",
+--	mine.delta_cases "Mío",
+	mine.mean_delta_cases "Mine",
+--	1e5 * mine.mean_delta_cases / 3.284e6 "Por 100k (Censo 2020)",
+	100.0 * (mine.mean_delta_cases / lag(mine.mean_delta_cases, 14) OVER (
+		ORDER BY bulletin_date
+	) - 1) AS "14-day change",
+	mine.mean_delta_first_infections "First cases",
+	mine.mean_delta_possible_reinfections "Possible reinfections"
+FROM official
+INNER JOIN mine
+	USING (bulletin_date)
+ORDER BY bulletin_date DESC;
