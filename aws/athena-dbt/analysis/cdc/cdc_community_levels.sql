e6dd0214731efcc1065cbfe6a8fa18db2bc6f587
sacundim <anybody@sacundim.org>
2022-04-22 16:37:46 -0700

    Redo CDC community levels query to use official case counts

--- aws/athena-dbt/analysis/cdc/cdc_community_levels.sql
+++ aws/athena-dbt/analysis/cdc/cdc_community_levels.sql
@@ -1,93 +1,72 @@
---
--- Draft implementation of of CDC Community Levels indicator
--- from Feburary 25, 2022
---
 WITH population AS (
- 	SELECT 3285874 AS pop2020
-), cumulative AS (
+ 	SELECT
+ 	    3193694 AS popest2019,
+ 	    3285874 AS census2020
+), cases AS (
+	SELECT 
+		bulletin_date AS date,
+		sum(delta_confirmed_cases) 
+			+ sum(delta_probable_cases)
+			AS cases
+	FROM {{ ref('bulletin_cases') }}
+	GROUP BY bulletin_date 
+), hosp AS (
+	WITH dates AS (
+		SELECT max(bulletin_date) AS bulletin_date
+		FROM {{ ref('hhs_hospitals_bitemporal') }}
+	)
 	SELECT
-		bulletin_date,
-		datum_date,
-		bioportal AS cases,
-		sum(bioportal) OVER (
-			PARTITION BY bulletin_date
-			ORDER BY datum_date
-		) AS cumulative_cases,
-		hospital_admissions AS admissions,
-		sum(hospital_admissions) OVER (
-			PARTITION BY bulletin_date
-			ORDER BY datum_date
-		) AS cumulative_admissions,
-		hospitalized_currently AS occupancy,
-		sum(hospitalized_currently) OVER (
-			PARTITION BY bulletin_date
-			ORDER BY datum_date
-		) AS cumulative_occupancy,
-		total_beds AS capacity,
-		sum(total_beds) OVER (
-			PARTITION BY bulletin_date
-			ORDER BY datum_date
-		) AS cumulative_capacity
-	FROM {{ ref('new_daily_cases') }}
-), seven AS (
-	SELECT
-		bulletin_date,
-		datum_date,
-		cumulative_cases - lag(cumulative_cases, 7) OVER (
-			PARTITION BY bulletin_date
-			ORDER BY datum_date
-		) cases,
-		cumulative_admissions - lag(cumulative_admissions, 7) OVER (
-			PARTITION BY bulletin_date
-			ORDER BY datum_date
-		) admissions,
-		cumulative_occupancy - lag(cumulative_occupancy, 7) OVER (
-			PARTITION BY bulletin_date
-			ORDER BY datum_date
-		) occupancy,
-		cumulative_capacity - lag(cumulative_capacity, 7) OVER (
-			PARTITION BY bulletin_date
-			ORDER BY datum_date
-		) capacity
-	FROM cumulative
-), per_capita AS (
-	SELECT
-		bulletin_date,
-		datum_date,
-		1e5 * cases / pop2020
-			AS cases,
-		1e5 * admissions / pop2020
+		date,
+		previous_day_admission_adult_covid 
+			+ previous_day_admission_pediatric_covid 
 			AS admissions,
-		100.0 * CAST(occupancy AS DOUBLE)
-			/ capacity
-			AS pct_occupancy
-	FROM seven
+		inpatient_beds_used_covid occupancy,
+		inpatient_beds capacity
+    FROM {{ ref('hhs_hospitals_bitemporal') }}
+	INNER JOIN dates USING (bulletin_date)
+), rates AS (
+	SELECT 
+		date,
+		1e5 * sum(cases) OVER (
+			ORDER BY date
+			ROWS 6 PRECEDING
+		) / popest2019 AS case_rate,
+		1e5 * sum(admissions) OVER (
+			ORDER BY date
+			ROWS 6 PRECEDING
+		) / popest2019 AS admission_rate,
+		100.0 * sum(occupancy) OVER (
+			ORDER BY date
+			ROWS 6 PRECEDING
+		) / sum(capacity) OVER (
+			ORDER BY date
+			ROWS 6 PRECEDING
+		) AS occupancy_pct
+	FROM cases
+	INNER JOIN hosp
+		USING (date)
 	CROSS JOIN population
 )
 SELECT
-	bulletin_date "Data until",
-	datum_date "Date",
-	cases "Case rate",
-	admissions "Hosp. admission rate",
-	pct_occupancy "Hospital occupation %",
-	CASE WHEN cases < 200
+	date "Date",
+	case_rate "Case rate",
+	admission_rate "Hosp. admission rate",
+	occupancy_pct "Hospital occupation %",
+	CASE WHEN case_rate < 200
 	THEN
 		CASE
-			WHEN admissions >= 20 OR pct_occupancy >= 15
+			WHEN admission_rate >= 20 OR occupancy_pct >= 15
 			THEN 'High'
-			WHEN admissions >= 10 OR pct_occupancy >= 10
+			WHEN admission_rate >= 10 OR occupancy_pct >= 10
 			THEN 'Medium'
 			ELSE 'Low'
 		END
 	ELSE
 		CASE
-			WHEN admissions >= 10 OR pct_occupancy >= 10
+			WHEN admission_rate >= 10 OR occupancy_pct >= 10
 			THEN 'High'
 			ELSE 'Medium'
 		END
 	END AS "Level"
-FROM per_capita
-WHERE bulletin_date = DATE '2022-02-24'
-AND datum_date >= DATE '2021-12-07'
-ORDER BY bulletin_date DESC, datum_date ASC;
-
+FROM rates
+ORDER BY date DESC;
