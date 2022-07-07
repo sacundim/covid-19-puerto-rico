dd83f440266e621af91ada96f033953777e6f4d2
sacundim <anybody@sacundim.org>
2022-07-07 15:12:58 -0700

    Job to download Walgreens/Aegis data (compiled by Andy Bloch)

--- Terraform/Batch.tf
+++ Terraform/Batch.tf
@@ -240,6 +240,82 @@ resource "aws_cloudwatch_event_target" "hhs_daily_download" {
 }
 
 
+
+#################################################################################
+#################################################################################
+##
+## Walgreens/Aegis daily download task
+##
+
+resource "aws_batch_job_definition" "walgreens_download_and_sync" {
+  name = "walgreens-download-and-sync"
+  tags = {
+    Project = var.project_name
+  }
+  propagate_tags = true
+  type = "container"
+  platform_capabilities = ["FARGATE"]
+
+  container_properties = jsonencode({
+    image = "${data.aws_ecr_image.downloader.registry_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/${data.aws_ecr_image.downloader.repository_name}:${data.aws_ecr_image.downloader.image_tag}"
+    command = ["andy-bloch-dashboard.sh"],
+    environment = [
+      {
+        name = "S3_DATA_URL",
+        value = "s3://${var.datalake_bucket_name}"
+      }
+    ],
+    executionRoleArn = aws_iam_role.ecs_task_role.arn
+    jobRoleArn = aws_iam_role.ecs_job_role.arn
+    fargatePlatformConfiguration = {
+      "platformVersion": "LATEST"
+    },
+    resourceRequirements = [
+      {"type": "VCPU", "value": "0.25"},
+      {"type": "MEMORY", "value": "512"}
+    ]
+    networkConfiguration = {
+      "assignPublicIp": "ENABLED"
+    }
+    "logConfiguration": {
+      "logDriver": "awslogs",
+      "options": {
+          "awslogs-group" = aws_cloudwatch_log_group.covid_19_puerto_rico.name,
+          "awslogs-region" = var.aws_region,
+          "awslogs-stream-prefix" = "walgreens-download-and-sync"
+      }
+    }
+  })
+
+  retry_strategy {
+    attempts = 1
+  }
+
+  timeout {
+    # 10 minutes, which is overkill
+    attempt_duration_seconds = 600
+  }
+}
+
+resource "aws_cloudwatch_event_rule" "walgreens_daily_download" {
+  name        = "walgreens-daily-download"
+  description = "Run the daily Walgreens download."
+  schedule_expression = "cron(25 16 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "walgreens_daily_download" {
+  target_id = "walgreens-daily-download"
+  rule = aws_cloudwatch_event_rule.walgreens_daily_download.name
+  arn = aws_batch_job_queue.batch_queue.arn
+  role_arn = aws_iam_role.ecs_events_role.arn
+
+  batch_target {
+    job_definition = aws_batch_job_definition.walgreens_download_and_sync.arn
+    job_name       = aws_batch_job_definition.walgreens_download_and_sync.name
+  }
+}
+
+
 ##############################################################################
 ##############################################################################
 ##
---
+++ scripts/andy-bloch-dashboard.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+#
+# Fetch the Walgrens/Aegis variant data from Andy Bloch's
+# "Walgreens COVID-19 Tracker Tracker." This is a dashboard
+# that scrapes data from Walgrens/Aegis's dashboard but
+# presents it better.
+#
+# * https://observablehq.com/@andy-bloch/walgreens-covid-19-tracker
+#
+
+set -eu
+set -o pipefail
+
+ENDPOINT="https://labvegas.com/data/covid19/walgreens/dashboard-Tracker_Aggregation.csv"
+timestamp="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
+BASENAME="dashboard-Tracker_Aggregation"
+
+HERE="$(dirname $0)"
+REPO_ROOT="${HERE}/.."
+TMP="${REPO_ROOT}/tmp"
+mkdir -p "${TMP}"
+
+S3_SYNC_DIR="${REPO_ROOT}/s3-bucket-sync/covid-19-puerto-rico-data"
+WALGREENS_SYNC_DIR="${S3_SYNC_DIR}/Walgreens"
+AGGREGATION_SYNC_DIR="${WALGREENS_SYNC_DIR}/Tracker_Aggregation"
+mkdir -p "${S3_SYNC_DIR}" "${WALGREENS_SYNC_DIR}" "${AGGREGATION_SYNC_DIR}"
+mkdir -p "${AGGREGATION_SYNC_DIR}"/csv/v1 "${AGGREGATION_SYNC_DIR}"/parquet/v1
+
+
+echo "$(date): Fetching from endpoint: ${ENDPOINT}"
+wget --no-verbose -O "${TMP}/${BASENAME}.csv" "${ENDPOINT}"
+echo "$(date): Downloaded to ${BASENAME}.csv"
+
+echo "$(date): Generating Parquet..."
+csv2parquet --codec gzip --row-group-size 10000000 \
+  "${TMP}/${BASENAME}.csv"
+
+echo "$(date): Compressing CSV..."
+bzip2 -9 "${TMP}/${BASENAME}.csv"
+
+echo "$(date): Moving to ${AGGREGATION_SYNC_DIR}..."
+mv "${TMP}/${BASENAME}.csv.bz2" \
+  "${AGGREGATION_SYNC_DIR}/csv/v1/${BASENAME}_${timestamp}.csv.bz2"
+mv "${TMP}/${BASENAME}.parquet" \
+  "${AGGREGATION_SYNC_DIR}/parquet/v1/${BASENAME}_${timestamp}.parquet"
+
+echo "$(date): Listing out ${AGGREGATION_SYNC_DIR}..."
+du -h "${AGGREGATION_SYNC_DIR}"/*
