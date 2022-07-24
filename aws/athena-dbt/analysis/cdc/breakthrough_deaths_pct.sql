2cbcba636a8d94702278a7a164de8f86bf37db2b
sacundim <anybody@sacundim.org>
2022-07-24 00:49:24 -0700

    Fix the age group granularity mismatch issue in `cdc_breakthroughs` by aggregating everything to the least common denominator between the three datase ts.

--- aws/athena-dbt/analysis/cdc/breakthrough_deaths_pct.sql
+++ aws/athena-dbt/analysis/cdc/breakthrough_deaths_pct.sql
@@ -1,20 +1,38 @@
 --
--- Weekly percentage of breakthrough deaths
+-- Weekly percentage of breakthrough deaths. Very roughly follows
+-- the following Kaiser Family Foundation report, although I was
+-- doing this sort of calculation before them, their is just very
+-- well disseminated so it's good to roughly follow that.
+--
+-- https://www.healthsystemtracker.org/brief/covid19-and-other-leading-causes-of-death-in-the-us/
 --
 SELECT
-	mmwr_week_start,
-	100.0 * primary_vaccinated_with_outcome / primary_with_outcome
-		AS breakthrough_pct,
-	100.0 * booster1_primary_series_only_with_outcome / booster1_with_outcome
-		AS primary_only_breakthrough_pct,
-	100.0 * booster1_boosted_with_outcome / booster1_with_outcome
-		AS boosted_breakthrough_pct,
-	100.0 * booster2_one_boosted_with_outcome / booster2_with_outcome
-		AS one_boosted_breakthrough_pct,
-	100.0 * booster2_two_boosted_with_outcome / booster2_with_outcome
-		AS two_boosted_breakthrough_pct
-FROM {{ ref("cdc_primary_series_breakthroughs") }} breakthrough
+	date_format(date_add('day', 6, mmwr_week_start), '%Y-%m') "Month",
+	sum(primary_population)
+		/ count(DISTINCT mmwr_week_start)
+		AS "Population in primary dataset",
+	100.0 * sum(primary_unvaccinated_with_outcome)
+		/ sum(primary_with_outcome)
+		AS "% no primary series",
+	100.0 * sum(primary_vaccinated_with_outcome)
+		/ sum(primary_with_outcome)
+		AS "% at least primary",
+	sum(booster1_population)
+		/ count(DISTINCT mmwr_week_start)
+		AS "Population in booster dataset",
+	100.0 * sum(booster1_primary_series_only_with_outcome)
+		/ sum(booster1_with_outcome)
+		AS "% only primary",
+	100.0 * sum(booster1_boosted_with_outcome)
+		/ sum(booster1_with_outcome)
+		AS "% at least one booster"
+FROM {{ ref("cdc_breakthroughs") }} breakthrough
 WHERE outcome = 'death'
-AND age_group = 'all_ages_adj'
 AND vaccine_product = 'all_types'
-ORDER BY mmwr_week_start DESC;
+-- We restrict to adults because that's what the widely-shared KFF chart
+-- does.  All three datasets cover only populations eligible for the vaccine
+-- dose that they concentrate on (primary, first booster, second booster)
+-- so that seems most useful.
+AND age_group != 'all_ages_adj' AND age_gte >= 18
+GROUP BY date_format(date_add('day', 6, mmwr_week_start), '%Y-%m')
+ORDER BY "Month" DESC;
--- aws/athena-dbt/models/hhs/staging/cdc_breakthroughs.sql
+++ aws/athena-dbt/models/hhs/staging/cdc_breakthroughs.sql
@@ -6,82 +6,110 @@
 -- dataset that in turn has more coverage than `booster2`.
 --
 SELECT
-	mmwr_week,
-	mmwr_week_start,
-	outcome,
-	age_group,
-	vaccine_product,
+	primary.mmwr_week,
+	primary.mmwr_week_start,
+	primary.outcome,
+	primary.vaccine_product,
+	age_dim.boosted_age_group
+	    AS age_group,
+    min(age_gte) age_gte,
+    max(age_lte) age_lte,
 
-	primary.vaccinated_with_outcome
+	sum(primary.vaccinated_with_outcome)
 	    AS primary_vaccinated_with_outcome,
-	primary.fully_vaccinated_population
+	sum(primary.fully_vaccinated_population)
 		AS primary_vaccinated_population,
-	primary.unvaccinated_with_outcome
+	sum(primary.unvaccinated_with_outcome)
 	    AS primary_unvaccinated_with_outcome,
-	primary.unvaccinated_population
+	sum(primary.unvaccinated_population)
 	    AS primary_unvaccinated_population,
-    primary.vaccinated_with_outcome
-        + primary.unvaccinated_with_outcome
+    sum(primary.vaccinated_with_outcome
+        + primary.unvaccinated_with_outcome)
         AS primary_with_outcome,
-	primary.fully_vaccinated_population
-	    + primary.unvaccinated_population
+	sum(primary.fully_vaccinated_population
+	    + primary.unvaccinated_population)
 	    AS primary_population,
 
-	booster1.boosted_with_outcome
+    -- The `booster1` and `booster2` are actually guaranteed
+    -- to have only one row per group from their source tables
+    -- so we have to use `arbitrary` to not count double.
+	arbitrary(booster1.boosted_with_outcome)
 	    AS booster1_boosted_with_outcome,
-	booster1.boosted_population
+	arbitrary(booster1.boosted_population)
 	    AS booster1_boosted_population,
-	booster1.primary_series_only_with_outcome
+	arbitrary(booster1.primary_series_only_with_outcome)
 	    AS booster1_primary_series_only_with_outcome,
-	booster1.primary_series_only_population
+	arbitrary(booster1.primary_series_only_population)
 	    AS booster1_primary_series_only_population,
-    booster1.unvaccinated_with_outcome
+    arbitrary(booster1.unvaccinated_with_outcome)
         AS booster1_unvaccinated_with_outcome,
-    booster1.unvaccinated_population
+    arbitrary(booster1.unvaccinated_population)
         AS booster1_unvaccinated_population,
-    booster1.boosted_with_outcome
+    arbitrary(booster1.boosted_with_outcome
 	    + booster1.primary_series_only_with_outcome
-	    + booster1.unvaccinated_with_outcome
+	    + booster1.unvaccinated_with_outcome)
 	    AS booster1_with_outcome,
-    booster1.boosted_population
+    arbitrary(booster1.boosted_population
 	    + booster1.primary_series_only_population
-	    + booster1.unvaccinated_population
+	    + booster1.unvaccinated_population)
 	    AS booster1_population,
 
-	booster2.one_boosted_with_outcome
+	arbitrary(booster2.one_boosted_with_outcome)
 	    AS booster2_one_boosted_with_outcome,
-	booster2.one_booster_population
+	arbitrary(booster2.one_booster_population)
 	    AS booster2_one_booster_population,
-	booster2.two_boosted_with_outcome
+	arbitrary(booster2.two_boosted_with_outcome)
 	    AS booster2_two_boosted_with_outcome,
-	booster2.two_booster_population
+	arbitrary(booster2.two_booster_population)
 	    AS booster2_two_booster_population,
-    booster2.vaccinated_with_outcome
+    arbitrary(booster2.vaccinated_with_outcome)
         AS booster2_primary_series_only_with_outcome,
-    booster2.fully_vaccinated_population
+    arbitrary(booster2.fully_vaccinated_population)
         AS booster2_primary_series_only_population,
-    booster2.unvaccinated_with_outcome
+    arbitrary(booster2.unvaccinated_with_outcome)
         AS booster2_unvaccinated_with_outcome,
-    booster2.unvaccinated_population
+    arbitrary(booster2.unvaccinated_population)
         AS booster2_unvaccinated_population,
-    booster2.one_boosted_with_outcome
+    arbitrary(booster2.one_boosted_with_outcome
 	    + booster2.two_boosted_with_outcome
 	    + booster2.vaccinated_with_outcome
-	    + booster2.unvaccinated_with_outcome
+	    + booster2.unvaccinated_with_outcome)
 	    AS booster2_with_outcome,
-    booster2.one_booster_population
+    arbitrary(booster2.one_booster_population
 	    + booster2.two_booster_population
 	    + booster2.fully_vaccinated_population
-	    + booster2.unvaccinated_population
+	    + booster2.unvaccinated_population)
 	    AS booster2_population
 FROM {{ ref("cdc_primary_series_breakthroughs") }} primary
+-- These data sets have different age groups granularities, with the
+-- primary one being more granular than the booster ones.  Here we
+-- match them up and aggregate to the least common denominator.
+INNER JOIN (VALUES ('5-11', 5, 11, '5-11'),
+				   ('12-17', 12, 17, '12-17'),
+				   ('18-29', 18, 29, '18-49'),
+				   ('30-49', 30, 49, '18-49'),
+				   ('50-64', 50, 64, '50-64'),
+				   ('65-79', 65, 79, '65+'),
+				   ('80+', 80, 200, '65+'),
+				   ('all_ages_adj', 0, 200, 'all_ages_adj'))
+				   AS age_dim (primary_age_group, age_gte, age_lte, boosted_age_group)
+	ON primary.age_group = age_dim.primary_age_group
 LEFT OUTER JOIN {{ ref("cdc_booster_breakthroughs") }} booster1
-	USING (mmwr_week, mmwr_week_start, outcome, age_group, vaccine_product)
+    ON booster1.mmwr_week = primary.mmwr_week
+    AND booster1.mmwr_week_start = primary.mmwr_week_start
+    AND booster1.outcome = primary.outcome
+    AND booster1.vaccine_product = primary.vaccine_product
+    AND booster1.age_group = age_dim.boosted_age_group
 LEFT OUTER JOIN {{ ref("cdc_second_booster_breakthroughs") }} booster2
-	USING (mmwr_week, mmwr_week_start, outcome, age_group, vaccine_product)
--- FIXME: As of 2022-07-23, these datasets don't actually join up on the
--- `age_group` column.  The primary dataset drills down into ages 18-29,
--- 30-49, 65-79 and 80+, but the breakthrough datasets aggregate to 18-49
--- and 65+.  So I'm restricting to 'all_ages_adj' for now.
-WHERE age_group = 'all_ages_adj'
+    ON booster2.mmwr_week = primary.mmwr_week
+    AND booster2.mmwr_week_start = primary.mmwr_week_start
+    AND booster2.outcome = primary.outcome
+    AND booster2.vaccine_product = primary.vaccine_product
+    AND booster2.age_group = age_dim.boosted_age_group
+GROUP BY
+	primary.mmwr_week,
+	primary.mmwr_week_start,
+	primary.outcome,
+	primary.vaccine_product,
+	age_dim.boosted_age_group
 ORDER BY mmwr_week_start;
