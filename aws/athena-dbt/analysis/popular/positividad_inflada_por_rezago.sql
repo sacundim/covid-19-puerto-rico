e80bac2f33303b4196a16001bd17fa95adb26b2f
sacundim <anybody@sacundim.org>
2022-08-11 10:27:54 -0700

    Analysis query to illustrate the problem when you pay too much attention to the most recent date on a test sample-date positivity chart—you get inflated % because the data is incomplete and the positive results get reported sooner.

---
+++ aws/athena-dbt/analysis/popular/positividad_inflada_por_rezago.sql
@@ -0,0 +1,31 @@
+WITH bulletins AS (
+	SELECT max(bulletin_date) AS bulletin_date
+	FROM {{ ref("bioportal_collected_agg") }}
+), bitemporal AS (
+	SELECT
+		bulletin_date,
+		collected_date,
+		100.0 * sum(positive_tests) OVER (
+		    PARTITION BY bulletin_date
+		    ORDER BY collected_date
+		    ROWS 6 PRECEDING
+		) / sum(tests) OVER (
+		    PARTITION BY bulletin_date
+		    ORDER BY collected_date
+		    ROWS 6 PRECEDING
+		) AS positive_rate
+	FROM {{ ref("bioportal_collected_agg") }}
+	WHERE test_type = 'Molecular'
+)
+SELECT
+	perspectival.bulletin_date AS "Fecha de muestras",
+	perspectival.positive_rate "Con datos solo hasta esa fecha",
+	collected.bulletin_date AS "Datos más recientes disponibles",
+	collected.positive_rate AS "Con datos más recientes disponibles"
+FROM bitemporal collected
+INNER JOIN bulletins
+	ON bulletins.bulletin_date = collected.bulletin_date
+INNER JOIN bitemporal perspectival
+	ON perspectival.bulletin_date = collected.collected_date
+	AND perspectival.collected_date = perspectival.bulletin_date
+ORDER BY "Fecha de muestras" DESC;
