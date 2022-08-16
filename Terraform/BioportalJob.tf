eaff39877715c2a203aad63a7e4f01687b2f5bb4
sacundim <anybody@sacundim.org>
2022-08-16 15:33:58 -0700

    Use just 2 vCPUs for Bioportal job; takes 16 minutes instead of 11, which is 50% longer but the 2 vCPU instance costs about half as much.

--- Terraform/BioportalJob.tf
+++ Terraform/BioportalJob.tf
@@ -25,7 +25,7 @@ resource "aws_batch_job_definition" "bioportal_download_and_sync" {
     executionRoleArn = aws_iam_role.ecs_task_role.arn
     jobRoleArn = aws_iam_role.ecs_job_role.arn
     resourceRequirements = [
-      {"type": "VCPU", "value": "4"},
+      {"type": "VCPU", "value": "2"},
       {"type": "MEMORY", "value": "6144"}
     ]
     "logConfiguration": {
