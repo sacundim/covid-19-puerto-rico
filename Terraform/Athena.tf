ba8413c05cea164fb905130d426d4b08409e72b5
sacundim <anybody@sacundim.org>
2023-02-18 01:49:20 -0800

    Upgrade Terraform AWS provider version

--- Terraform/Athena.tf
+++ Terraform/Athena.tf
@@ -13,3 +13,18 @@ resource "aws_athena_workgroup" "main" {
     }
   }
 }
+
+resource "aws_athena_workgroup" "dbt" {
+  // DBT doesn't like the `output_location` setting in our main
+  // workground, so we make a whole nother one for it
+  name = "${var.project_name}-dbt"
+  tags = {
+    Project = var.project_name
+  }
+
+  configuration {
+    engine_version {
+      selected_engine_version = "AUTO"
+    }
+  }
+}
