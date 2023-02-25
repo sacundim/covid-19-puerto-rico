f22271c03c9bf5957e793d968a063bf3f01cc324
sacundim <anybody@sacundim.org>
2023-02-24 22:21:35 -0800

    Upgrade to Athena engine version 3

--- Terraform/Athena.tf
+++ Terraform/Athena.tf
@@ -6,7 +6,7 @@ resource "aws_athena_workgroup" "main" {
 
   configuration {
     engine_version {
-      selected_engine_version = "AUTO"
+      selected_engine_version = "Athena engine version 3"
     }
     result_configuration {
       output_location = "s3://${aws_s3_bucket.athena_bucket.bucket}/"
@@ -16,7 +16,7 @@ resource "aws_athena_workgroup" "main" {
 
 resource "aws_athena_workgroup" "dbt" {
   // DBT doesn't like the `output_location` setting in our main
-  // workground, so we make a whole nother one for it
+  // workgroup, so we make a whole nother one for it
   name = "${var.project_name}-dbt"
   tags = {
     Project = var.project_name
@@ -24,7 +24,7 @@ resource "aws_athena_workgroup" "dbt" {
 
   configuration {
     engine_version {
-      selected_engine_version = "AUTO"
+      selected_engine_version = "Athena engine version 3"
     }
   }
 }
