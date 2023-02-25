c7d15f7cf5968eee702fee3e3db1d99c7c668e07
sacundim <anybody@sacundim.org>
2023-02-24 18:27:06 -0800

    Upgrade Terraform AWS provider to 4.56.0

--- Terraform/AWS.tf
+++ Terraform/AWS.tf
@@ -2,7 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.55.0"
+      version = "~> 4.56.0"
+      version = "~> 4.56.0"
     }
   }
 }
