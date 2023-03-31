c78d68ea4d6bacc57167aaafd409f0324e73920c
sacundim <anybody@sacundim.org>
2023-03-30 18:44:21 -0700

    Add a AAAA DNS record for the website

--- Terraform/Cloudfront.tf
+++ Terraform/Cloudfront.tf
@@ -161,3 +161,15 @@ resource "aws_route53_record" "root-a" {
     evaluate_target_health = false
   }
 }
+
+resource "aws_route53_record" "root-aaaa" {
+  zone_id = aws_route53_zone.dns_zone.zone_id
+  name = var.dns_name
+  type = "AAAA"
+
+  alias {
+    name = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
