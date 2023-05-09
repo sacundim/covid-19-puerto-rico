b3e79d5c54b18685cecfbe113e8ee2d5cb9a2241
sacundim <anybody@sacundim.org>
2023-05-08 18:49:21 -0700

    External table definitions for Biostatistics data sets

---
+++ aws/athena/create-biostatistics-schema.sql
@@ -0,0 +1,135 @@
+--
+-- Create the schema that reads from the Biostatistics source data files:
+--
+-- * https://biostatistics.salud.pr.gov/swagger/index.html
+--
+-- We make zero effort here to clean these files or even
+-- to parse the data types and treat them as strings unless
+-- it's zero effort to coerce them to right type.
+--
+
+DROP DATABASE IF EXISTS biostatistics_sources CASCADE;
+
+CREATE DATABASE biostatistics_sources
+LOCATION 's3://covid-19-puerto-rico-data/biostatistics.salud.pr.gov/';
+
+
+
+--------------------------------------------------------------------------
+--------------------------------------------------------------------------
+--
+-- Data Sources
+--
+
+CREATE EXTERNAL TABLE biostatistics_sources.data_sources_parquet_v1 (
+    id STRING,
+    name STRING,
+    recordCount BIGINT,
+    lastUpdated STRING,
+    originTimeZone STRING
+) PARTITIONED BY (downloaded_date STRING)
+STORED AS PARQUET
+LOCATION 's3://covid-19-puerto-rico-data/biostatistics.salud.pr.gov/data-sources/parquet_v1/';
+
+
+
+--------------------------------------------------------------------------
+--------------------------------------------------------------------------
+--
+-- Deaths
+--
+
+CREATE EXTERNAL TABLE biostatistics_sources.deaths_parquet_v1 (
+    deathId STRING,
+    deathDate STRING,
+    deathReportDate STRING,
+    sex STRING,
+    ageRange STRING,
+    physicalRegion STRING,
+    vaccinationStatusAtDeath STRING
+) PARTITIONED BY (downloaded_date STRING)
+STORED AS PARQUET
+LOCATION 's3://covid-19-puerto-rico-data/biostatistics.salud.pr.gov/deaths/parquet_v1/';
+
+
+--------------------------------------------------------------------------
+--------------------------------------------------------------------------
+--
+-- Cases
+--
+
+CREATE EXTERNAL TABLE biostatistics_sources.cases_parquet_v1 (
+    caseId STRING,
+    caseCategory STRING,
+    caseType STRING,
+    caseClassification STRING,
+    patientId STRING,
+    patientAgeRange STRING,
+    patientSex STRING,
+    patientPhysicalCity STRING,
+    patientPhysicalRegion STRING,
+    earliestPositiveRankingTestSampleCollectedDate STRING,
+    earliestPositiveDiagnosticTestSampleCollectedDate STRING,
+    caseCreatedAt STRING
+) PARTITIONED BY (downloaded_date STRING)
+STORED AS PARQUET
+LOCATION 's3://covid-19-puerto-rico-data/biostatistics.salud.pr.gov/cases/parquet_v1/';
+
+
+
+--------------------------------------------------------------------------
+--------------------------------------------------------------------------
+--
+-- Test orders
+--
+
+CREATE EXTERNAL TABLE biostatistics_sources.tests_parquet_v1 (
+    orderTestId STRING,
+    patientId STRING,
+    patientAgeRange STRING,
+    patientRegion STRING,
+    patientCity STRING,
+    orderTestCategory STRING,
+    orderTestType STRING,
+    sampleCollectedDate STRING,
+    resultReportDate STRING,
+    orderTestResult STRING,
+    orderTestCreatedAt STRING
+) PARTITIONED BY (downloaded_date STRING)
+STORED AS PARQUET
+LOCATION 's3://covid-19-puerto-rico-data/biostatistics.salud.pr.gov/tests/parquet_v1/';
+
+
+CREATE EXTERNAL TABLE biostatistics_sources.tests_grouped_parquet_v1 (
+    sampleCollectedDate STRING,
+    entity STRING,
+    entityCity STRING,
+    totalTestsProcessed BIGINT,
+    totalMolecularTestsProcessed BIGINT,
+    totalMolecularTestsPositive BIGINT,
+    totalMolecularTestsNegative BIGINT,
+    totalAntigensTestsProcessed BIGINT,
+    totalAntigensTestsPositive BIGINT,
+    totalAntigensTestsNegative BIGINT
+) PARTITIONED BY (downloaded_date STRING)
+STORED AS PARQUET
+LOCATION 's3://covid-19-puerto-rico-data/biostatistics.salud.pr.gov/tests-grouped/parquet_v1/';
+
+
+--------------------------------------------------------------------------
+--------------------------------------------------------------------------
+--
+-- Vaccination
+--
+
+CREATE EXTERNAL TABLE biostatistics_sources.persons_with_vax_status_parquet_v1 (
+    personAgeRange STRING,
+    personSex STRING,
+    personState STRING,
+    personCity STRING,
+    personRegion STRING,
+    personVaccinationStatus STRING,
+    personLastVaccinationDate STRING
+) PARTITIONED BY (downloaded_date STRING)
+STORED AS PARQUET
+LOCATION 's3://covid-19-puerto-rico-data/biostatistics.salud.pr.gov/persons-with-vaccination-status/parquet_v1/';
