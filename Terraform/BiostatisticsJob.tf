a8386d749d8d9bcd28661cc925b7b58c39d04a9d
sacundim <anybody@sacundim.org>
2023-05-20 22:06:26 -0700

    Switching away from the DuckDB binary UUID type apparently bumped our memory requirements quite a bit and the OOM killer punished us for it. Bump memory.

--- Terraform/BiostatisticsJob.tf
+++ Terraform/BiostatisticsJob.tf
@@ -29,8 +29,8 @@ resource "aws_batch_job_definition" "biostatistics_download_and_sync" {
     executionRoleArn = aws_iam_role.ecs_task_role.arn
     jobRoleArn = aws_iam_role.ecs_job_role.arn
     resourceRequirements = [
-      {"type": "VCPU", "value": "2"},
-      {"type": "MEMORY", "value": "6144"}
+      {"type": "VCPU", "value": "4"},
+      {"type": "MEMORY", "value": "14336"}
     ]
     "logConfiguration": {
       "logDriver": "awslogs",
