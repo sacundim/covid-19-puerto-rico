32a28b8b487a719fb666ae8314f202d36318ec28
sacundim <anybody@sacundim.org>
2023-05-22 12:04:21 -0700

    Staging models for four Biostatistics datasets

---
+++ aws/athena-dbt/models/biostatistics/staging/biostatistics_persons_with_vax_status.sql
@@ -0,0 +1,20 @@
+{{
+    config(pre_hook=[
+        "MSCK REPAIR TABLE {{ source('biostatistics', 'persons_with_vax_status_v2').render_hive() }}"
+    ])
+}}
+SELECT
+    date(downloaded_date) AS downloaded_date,
+    downloadedAt AS downloaded_at,
+    CAST(downloadedAt AT TIME ZONE 'America/Puerto_Rico' AS DATE)
+        - INTERVAL '1' DAY
+        AS bulletin_date,
+    {{ clean_age_range('personAgeRange') }} AS age_range,
+    personSex AS sex,
+    personState AS state,
+    {{ clean_region('personRegion') }} AS region,
+    {{ clean_municipality('personCity') }} AS municipality,
+    personVaccinationStatus AS vax_status,
+    personLastVaccinationDate AS last_vax_date
+FROM {{ source('biostatistics', 'persons_with_vax_status_v2') }}
+ORDER BY downloaded_at, last_vax_date;
