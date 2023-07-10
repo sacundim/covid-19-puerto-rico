e63188ebaf0876373c14942c07c0f009a7c9fd4b
sacundim <anybody@sacundim.org>
2023-07-09 21:07:15 -0700

    Iceberg: Covid19Datos tables

--- aws/athena-dbt/models/covid19datos_v2/staging/casos.sql
+++ aws/athena-dbt/models/covid19datos_v2/staging/casos.sql
@@ -3,14 +3,26 @@
 --
 -- Simple cleanup of the cases dataset.
 --
-
 {{
-    config(pre_hook=[
-        "MSCK REPAIR TABLE {{ source('covid19datos_v2', 'casos_v1').render_hive() }}",
-        "MSCK REPAIR TABLE {{ source('covid19datos_v2', 'casos_v2').render_hive() }}",
+    config(
+      pre_hook=[
         "MSCK REPAIR TABLE {{ source('covid19datos_v2', 'casos_v3').render_hive() }}"
-    ])
+      ],
+      table_type='iceberg',
+      partitioned_by=['month(downloaded_date)'],
+      materialized='incremental',
+      unique_key=['downloaded_at', 'id_number'],
+      incremental_strategy='append'
+    )
 }}
+{% if is_incremental() %}
+WITH incremental AS (
+    SELECT
+        max(downloaded_at) max_downloaded_at,
+        CAST(max(downloaded_date) AS VARCHAR) max_downloaded_date
+    FROM {{ this }}
+)
+{% endif %}
 
 SELECT
     {{ parse_filename_timestamp('"$path"') }}
@@ -26,6 +38,12 @@ SELECT
 	from_iso8601_date(downloaded_date)
 		AS downloaded_date
 FROM {{ source('covid19datos_v2', 'casos_v1') }}
+{% if is_incremental() %}
+INNER JOIN incremental
+  ON {{ parse_filename_timestamp('"$path"') }} > max_downloaded_at
+  -- IMPORTANT: prunes partitions
+  AND downloaded_date >= max_downloaded_date
+{% endif %}
 
 UNION ALL
 
@@ -43,6 +61,12 @@ SELECT
 	from_iso8601_date(downloaded_date)
 		AS downloaded_date
 FROM {{ source('covid19datos_v2', 'casos_v2') }}
+{% if is_incremental() %}
+INNER JOIN incremental
+  ON {{ parse_filename_timestamp('"$path"') }} > max_downloaded_at
+  -- IMPORTANT: prunes partitions
+  AND downloaded_date >= max_downloaded_date
+{% endif %}
 
 UNION ALL
 
@@ -59,4 +83,10 @@ SELECT
 		AS sample_date,
 	from_iso8601_date(downloaded_date)
 		AS downloaded_date
-FROM {{ source('covid19datos_v2', 'casos_v3') }};
+FROM {{ source('covid19datos_v2', 'casos_v3') }}
+{% if is_incremental() %}
+INNER JOIN incremental
+  ON {{ parse_filename_timestamp('"$path"') }} > max_downloaded_at
+  -- IMPORTANT: prunes partitions
+  AND downloaded_date >= max_downloaded_date
+{% endif %}
--- aws/athena-dbt/models/covid19datos_v2/staging/pruebas.sql
+++ aws/athena-dbt/models/covid19datos_v2/staging/pruebas.sql
@@ -1,9 +1,25 @@
 {{
-    config(pre_hook=[
+    config(
+      pre_hook=[
         "MSCK REPAIR TABLE {{ source('covid19datos_v2', 'pruebas_v3').render_hive() }}"
-    ])
+      ],
+      table_type='iceberg',
+      partitioned_by=['month(downloaded_date)'],
+      materialized='incremental',
+      unique_key=['downloaded_at', 'id_orden'],
+      incremental_strategy='append'
+    )
 }}
+{% if is_incremental() %}
+WITH incremental AS (
+    SELECT
+        max(downloaded_at) max_downloaded_at,
+        CAST(max(downloaded_date) AS VARCHAR) max_downloaded_date
+    FROM {{ this }}
+)
+{% endif %}
 
+ /* The initial full refresh times out in Athena when I try to do v1, v2 and v3
 SELECT
 	from_iso8601_date(downloaded_date)
 		AS downloaded_date,
@@ -18,8 +34,12 @@ SELECT
  	date(date_parse(NULLIF(fe_prueba, ''), '%Y-%m-%d %H:%i:%s'))
 		AS fe_prueba
 FROM {{ source('covid19datos_v2', 'pruebas_v1') }}
--- IMPORTANT: This prunes partitions
-WHERE downloaded_date >= cast(date_add('day', -17, current_date) AS VARCHAR)
+{% if is_incremental() %}
+INNER JOIN incremental
+  ON {{ parse_filename_timestamp('"$path"') }} > max_downloaded_at
+  -- IMPORTANT: prunes partitions
+  AND downloaded_date >= max_downloaded_date
+{% endif %}
 
 UNION ALL
 
@@ -37,10 +57,15 @@ SELECT
  	date(date_parse(NULLIF(fe_prueba, ''), '%Y-%m-%d %H:%i:%s'))
 		AS fe_prueba
 FROM {{ source('covid19datos_v2', 'pruebas_v2') }}
--- IMPORTANT: This prunes partitions
-WHERE downloaded_date >= cast(date_add('day', -17, current_date) AS VARCHAR)
+{% if is_incremental() %}
+INNER JOIN incremental
+  ON {{ parse_filename_timestamp('"$path"') }} > max_downloaded_at
+  -- IMPORTANT: prunes partitions
+  AND downloaded_date >= max_downloaded_date
+{% endif %}
 
 UNION ALL
+*/
 
 SELECT
 	from_iso8601_date(downloaded_date)
@@ -56,5 +81,9 @@ SELECT
  	date(date_parse(NULLIF(fe_prueba, ''), '%Y-%m-%d %H:%i:%s'))
 		AS fe_prueba
 FROM {{ source('covid19datos_v2', 'pruebas_v3') }}
--- IMPORTANT: This prunes partitions
-WHERE downloaded_date >= cast(date_add('day', -17, current_date) AS VARCHAR);
+{% if is_incremental() %}
+INNER JOIN incremental
+  ON {{ parse_filename_timestamp('"$path"') }} > max_downloaded_at
+  -- IMPORTANT: prunes partitions
+  AND downloaded_date >= max_downloaded_date
+{% endif %}
