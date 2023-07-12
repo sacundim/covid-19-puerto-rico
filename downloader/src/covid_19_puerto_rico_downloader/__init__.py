645a8d51d22a2cb19fcfd45d4bce3876f7b1b05c
sacundim <anybody@sacundim.org>
2023-07-12 12:53:55 -0700

    Python/DuckDB capture of Andy Bloch's Walgreens dashboard dataset.

--- downloader/src/covid_19_puerto_rico_downloader/__init__.py
+++ downloader/src/covid_19_puerto_rico_downloader/__init__.py
@@ -1,3 +1,4 @@
 from .biostatistics import biostatistics
 from .covid19datos_v2 import covid19datos_v2
 from .hhs import hhs_download
+from .walgreens import walgreens
--- downloader/src/covid_19_puerto_rico_downloader/walgreens.py
+++ downloader/src/covid_19_puerto_rico_downloader/walgreens.py
@@ -1,5 +1,4 @@
 import argparse
-import concurrent.futures as futures
 import datetime
 import logging
 from threading import Lock
@@ -7,34 +6,22 @@ from threading import Lock
 from . import task
 from . import util
 
+ENDPOINT = 'https://labvegas.com/data/covid19/walgreens/'
 
 def process_arguments():
-    parser = argparse.ArgumentParser(description='Download PRDoH Covid19Datos V2 data sets')
+    parser = argparse.ArgumentParser(description='Download Andy Bloch Walgreens dataset')
     parser.add_argument('--s3-sync-dir', type=str, required=True,
                         help='Override for directory to which to deposit the output files for sync')
     parser.add_argument('--endpoint-url', type=str, default=ENDPOINT,
-                        help='Override for the URL of the Covid19Datos V2 API endpoint root.')
+                        help='Override for the URL of the API endpoint root.')
     parser.add_argument('--bzip2-command', type=str, default='lbzip2',
                         help='Override the command used to do bzip2 compression. Default: `lbzip2`.')
-    parser.add_argument('--duckdb-file', type=str, default='Covid19Datos-V2.duckdb',
-                        help='Override name of the DuckDB database file. Default: `Covid19Datos-V2.duckdb`.')
+    parser.add_argument('--duckdb-file', type=str, default='Walgreens.duckdb',
+                        help='Override name of the DuckDB database file. Default: `Walgreens.duckdb`.')
     return parser.parse_args()
 
-ENDPOINT='https://covid19datos.salud.pr.gov/estadisticas_v2/download/data'
 
-DATASETS = [
-    # Put larger ones first
-    'pruebas',
-    'casos',
-    'vacunacion',
-    'defunciones',
-    'sistemas_salud',
-    'vigilancia',
-]
-
-
-def covid19datos_v2():
-    """Entry point for PRDoH Covid19Datos V2 download code."""
+def walgreens():
     logging.basicConfig(
         format='%(asctime)s %(threadName)s %(message)s',
         level=logging.INFO)
@@ -45,28 +32,24 @@ def covid19datos_v2():
         extension='csv',
         http=util.make_requests_session('application/csv'),
         duck=util.make_duckdb_connection(args.duckdb_file),
-        jinja=util.make_jinja('covid19datos-v2'),
+        jinja=util.make_jinja('walgreens'),
         # We don't want to cause a big spike on the Covid19Datos servers
         # by going off and downloading all their datasets in parallel.  So
         # we use this mutex to make sure only one download goes at a time.
         mutex=Lock(),
         endpoint_url=args.endpoint_url,
         s3_sync_dir=args.s3_sync_dir,
-        endpoint_dir_name='covid19datos-v2',
-        input_dir_name='csv_v3',
-        parquet_dir_name='parquet_v4',
+        endpoint_dir_name='Walgreens',
+        input_dir_name='csv_v1',
+        parquet_dir_name='parquet_v2',
         ts_format='%Y-%m-%dT%H:%M:%SZ'
     )
 
-    tasks = [
-        task.Task(dataset, f'{dataset}/completo', config)
-        for dataset in DATASETS
-    ]
-    with futures.ThreadPoolExecutor(
-            max_workers=len(tasks),
-            thread_name_prefix='download_task') as executor:
-        for future in futures.as_completed([executor.submit(task) for task in tasks]):
-            logging.info("Completed %s", future.result())
+    dataset = 'Tracker_Aggregation'
+    the_task = task.Task(dataset, f'dashboard-Tracker_Aggregation.csv', config)
+    the_task()
+    logging.info("Completed")
+
 
 def pick_and_log_now():
     now = datetime.datetime.utcnow()
