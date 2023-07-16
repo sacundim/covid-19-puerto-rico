c367f8c23b03958b4630efc91224b2ca14732da3
sacundim <anybody@sacundim.org>
2023-07-15 23:46:07 -0700

    Fix copy-paste errors in shell script

--- athena-dbt/run-in-docker.sh
+++ athena-dbt/run-in-docker.sh
@@ -4,9 +4,9 @@ set -euxo pipefail
 # Overrides for AWS parameters
 AWS_REGION="${AWS_REGION:=us-west-2}"
 ATHENA_S3_SCHEMA="${ATHENA_S3_SCHEMA:=covid19_puerto_rico_iceberg}"
-ATHENA_WORK_GROUP="${ATHENA_WORK_GROUP:=covid-19-puerto-rico}"
+ATHENA_WORK_GROUP="${ATHENA_WORK_GROUP:=covid-19-puerto-rico-dbt}"
 ATHENA_S3_STAGING_DIR="${ATHENA_S3_STAGING_DIR:=s3://covid-19-puerto-rico-athena/}"
-ATHENA_S3_DATA_DIR="${ATHENA_S3_STAGING_DIR:=s3://covid-19-puerto-rico-iceberg/}"
+ATHENA_S3_DATA_DIR="${ATHENA_S3_DATA_DIR:=s3://covid-19-puerto-rico-iceberg/}"
 
 # Override to use a different image name or tag
 IMAGE_NAME="${IMAGE_NAME-docker.io/sacundim/covid-19-puerto-rico-dbt}"
