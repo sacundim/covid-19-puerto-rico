cde8f1702c594cc89168acafc1ec300c4a250067
sacundim <anybody@sacundim.org>
2023-07-23 20:10:06 -0700

    Making Docker images in a Nix flake is complicated, because if you're in a Darwin host, by default it tries to build a Darwin image instead of a Linux image

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/22.11";
     flake-utils.url = "github:numtide/flake-utils";
+
     poetry2nix = {
       url = "github:nix-community/poetry2nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -11,41 +12,80 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication;
-        pkgs = nixpkgs.legacyPackages.${system}.pkgs;
-      in rec {
-        packages = {
-          downloader = mkPoetryApplication {
-            name = "covid-19-puerto-rico-downloader";
-            projectDir = ./downloader;
-            python = pkgs.python311;
-            preferWheels = true;
-            propagatedBuildInputs = with pkgs; [
-              lbzip2 rclone
-            ];
-          };
+    let
+      baseOutputs = flake-utils.lib.eachDefaultSystem (system:
+        let
+          inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication;
+          pkgs = nixpkgs.legacyPackages.${system}.pkgs;
+        in rec {
+          packages = {
+            downloader = mkPoetryApplication {
+              name = "covid-19-puerto-rico-downloader";
+              projectDir = ./downloader;
+              python = pkgs.python311;
+              preferWheels = true;
+              propagatedBuildInputs = with pkgs; [
+                lbzip2 rclone
+              ];
+            };
 
-          website = mkPoetryApplication {
-            name = "covid-19-puerto-rico-website";
-            projectDir = ./website;
-            python = pkgs.python311;
-            preferWheels = true;
-            propagatedBuildInputs = with pkgs; [
-              rclone
-            ];
+            website = mkPoetryApplication {
+              name = "covid-19-puerto-rico-website";
+              projectDir = ./website;
+              python = pkgs.python311;
+              preferWheels = true;
+              propagatedBuildInputs = with pkgs; [
+                rclone
+              ];
 
-          };
+            };
 
-          default = pkgs.symlinkJoin {
-            name = "covid-19-puerto-rico";
-            paths = [
-              packages.downloader
-              packages.website
-            ];
+            default = pkgs.symlinkJoin {
+              name = "covid-19-puerto-rico";
+              paths = [
+                packages.downloader
+                packages.website
+              ];
+            };
           };
-        };
-      }
-    );
+        }
+      );
+
+      dockerImages = flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system}.pkgs;
+        in {
+          packages = {
+            downloader-docker = pkgs.dockerTools.streamLayeredImage {
+              name = "docker.io/sacundim/covid-19-puerto-rico-downloader";
+              tag = "latest-nix-${system}";
+              contents = [ baseOutputs.packages.${system}.downloader ];
+            };
+
+            website-docker = pkgs.dockerTools.streamLayeredImage {
+              name = "docker.io/sacundim/covid-19-puerto-rico-website";
+              tag = "latest-nix-${system}";
+              contents = [ baseOutputs.packages.${system}.website ];
+              config = {
+                Cmd = [ "covid19pr" ];
+              };
+            };
+          };
+        }
+      );
+    in {
+      packages = baseOutputs.packages
+              // dockerImages.packages
+
+              # TODO: a Linux VM that can run in Darwin from within which I can build
+              # TODO: the Docker images
+              #
+              # NIXWTF: why do I have to do this?  Because the nixpkgs#darwin.builder
+              # NIXWTF: doesn't have enough RAM (I get exit 126 errors), and if I'm
+              # NIXWTF: going to have to go to hell and back to get that to work I'm
+              # NIXWTF: going to frickin' have it committed here, where it doesn't belong.
+#              // darwinBuilder.packages
+              ;
+    };
+
 }
