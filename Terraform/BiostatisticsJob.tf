d8453648f04f2e2364ddd6148b63c2ff1c9ca4e7
sacundim <anybody@sacundim.org>
2023-07-24 16:00:36 -0700

    Tried the BiostatisticsJob on Fargate instead of EC2 (both Graviton); it goes from ~11:30 minutes to ~13:30.  Not worth it, but checking in to a branch for reference.

--- Terraform/BiostatisticsJob.tf
+++ Terraform/BiostatisticsJob.tf
@@ -11,7 +11,7 @@ resource "aws_batch_job_definition" "biostatistics_download_and_sync" {
   }
   propagate_tags = true
   type = "container"
-  platform_capabilities = ["EC2"]
+  platform_capabilities = ["FARGATE"]
 
   parameters = {
     "rclone_destination": ":s3,provider=AWS,env_auth:${var.datalake_bucket_name}"
@@ -30,13 +30,29 @@ resource "aws_batch_job_definition" "biostatistics_download_and_sync" {
         value = var.biostatistics_api_url
       }
     ],
-
-    executionRoleArn = aws_iam_role.ecs_task_role.arn
-    jobRoleArn = aws_iam_role.ecs_job_role.arn
     resourceRequirements = [
       {"type": "VCPU", "value": "2"},
       {"type": "MEMORY", "value": "6144"}
     ]
+    "ephemeralStorage" = {
+      "sizeInGiB": 40
+    }
+
+    executionRoleArn = aws_iam_role.ecs_task_role.arn
+    jobRoleArn = aws_iam_role.ecs_job_role.arn
+
+    fargatePlatformConfiguration = {
+      "platformVersion": "LATEST"
+    },
+    runtimePlatform = {
+      operatingSystemFamily: "LINUX",
+      cpuArchitecture: "ARM64"
+    }
+
+    networkConfiguration = {
+      "assignPublicIp": "ENABLED"
+    }
+
     "logConfiguration": {
       "logDriver": "awslogs",
       "options": {
@@ -66,7 +82,7 @@ resource "aws_cloudwatch_event_rule" "biostatistics_daily_download" {
 resource "aws_cloudwatch_event_target" "biostatistics_daily_download" {
   target_id = "biostatistics-daily-download"
   rule = aws_cloudwatch_event_rule.biostatistics_daily_download.name
-  arn = aws_batch_job_queue.ec2_arm64.arn
+  arn = aws_batch_job_queue.fargate_amd64.arn
   role_arn = aws_iam_role.ecs_events_role.arn
 
   batch_target {
