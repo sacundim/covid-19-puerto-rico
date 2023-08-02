238645b1c2282e14dc7e6b581d989b798979f73d
sacundim <anybody@sacundim.org>
2023-08-02 01:18:05 -0700

    Ooops, I messed up IAM permissions

--- Terraform/Workflow.tf
+++ Terraform/Workflow.tf
@@ -449,14 +449,7 @@ resource "aws_iam_policy" "sfn_run_batch_job_sync" {
           "batch:DescribeJobs",
           "batch:TerminateJob"
         ],
-        "Resource": [
-          aws_batch_job_definition.biostatistics_download_and_sync.arn,
-          aws_batch_job_definition.covid19datos_v2_download_and_sync.arn,
-          aws_batch_job_definition.dbt_run_models.arn,
-          aws_batch_job_definition.hhs_download_and_sync.arn,
-          aws_batch_job_definition.walgreens_download_and_sync.arn,
-          aws_batch_job_definition.website_generator.arn
-        ]
+        "Resource": "*"
       },
       {
         "Effect": "Allow",
